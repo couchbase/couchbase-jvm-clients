c3766a6ae74a424530d49f4a993dcce995df1b24
Michael Nitschinger <michael@nitschinger.at>
2018-09-28 10:10:57 +0200

    Initial Repository Setup

---
+++ kotlin-client/build.gradle.kts
@@ -0,0 +1,7 @@
+plugins {
+  kotlin("jvm") version "1.2.71"
+}
+
+dependencies {
+  implementation(kotlin("stdlib", "1.2.71"))
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,6 @@
+rootProject.name = "couchbase-jvm-clients"
+
+include("core-io")
+include("java-client")
+include("scala-client")
+include("kotlin-client")
