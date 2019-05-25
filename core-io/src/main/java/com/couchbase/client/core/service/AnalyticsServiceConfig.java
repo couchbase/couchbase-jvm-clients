6d9584a03b736a1048a82f1b8866ccbc46af7bab
Michael Nitschinger <michael@nitschinger.at>
2019-05-25 12:44:29 +0200

    core: add static builders to service configs
    Change-Id: I372685185e9c954d6dbf8962086000e4f0c99321
    Reviewed-on: http://review.couchbase.org/109662
    Reviewed-by: Michael Nitschinger <michael.nitschinger@couchbase.com>
    Tested-by: Michael Nitschinger <michael.nitschinger@couchbase.com>

--- core-io/src/main/java/com/couchbase/client/core/service/AnalyticsServiceConfig.java
+++ core-io/src/main/java/com/couchbase/client/core/service/AnalyticsServiceConfig.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2019 Couchbase, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.couchbase.client.core.service;
 
 import java.time.Duration;
@@ -13,6 +29,18 @@ public class AnalyticsServiceConfig extends AbstractPooledEndpointServiceConfig 
       .minEndpoints(DEFAULT_MIN_ENDPOINTS)
       .maxEndpoints(DEFAULT_MAX_ENDPOINTS)
       .idleTime(DEFAULT_IDLE_TIME);
+  }
+
+  public static Builder minEndpoints(int minEndpoints) {
+    return builder().minEndpoints(minEndpoints);
+  }
+
+  public static Builder maxEndpoints(int maxEndpoints) {
+    return builder().maxEndpoints(maxEndpoints);
+  }
+
+  public static Builder idleTime(Duration idleTime) {
+    return builder().idleTime(idleTime);
   }
 
   private AnalyticsServiceConfig(Builder builder) {
