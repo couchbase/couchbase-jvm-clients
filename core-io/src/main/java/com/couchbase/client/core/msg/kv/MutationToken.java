ce73df50a2fb0209198d4eaa813550428dd20266
David Nault <david.nault@couchbase.com>
2019-06-04 14:23:07 -0700

    Gardening: Fix Findbug warnings about MutationToken.hashCode()
    Fix bad shifts and useless self-computation.
    
    While we're making changes, simplify the toString() method and also
    regenerate the equals() method to take advantage of Objects.equals()
    for checking nullable fields.
    
    Change-Id: I968e73b5a159dfd105ada0040059e73c18a3096a
    Reviewed-on: http://review.couchbase.org/110163
    Tested-by: Build Bot <build@couchbase.com>
    Reviewed-by: David Nault <david.nault@couchbase.com>

--- core-io/src/main/java/com/couchbase/client/core/msg/kv/MutationToken.java
+++ core-io/src/main/java/com/couchbase/client/core/msg/kv/MutationToken.java
@@ -15,73 +15,70 @@
  */
 package com.couchbase.client.core.msg.kv;
 
+import java.util.Objects;
+
 /**
  * Value object to contain vbucket UUID and sequence number.
  *
- * @author Michael Nitschinger
  * @since 1.2.0
  */
 public class MutationToken {
 
-    private final short vbucketID;
-    private final long vbucketUUID;
-    private final long sequenceNumber;
-    private final String bucket;
+  private final short vbucketID;
+  private final long vbucketUUID;
+  private final long sequenceNumber;
+  private final String bucket;
 
-    public MutationToken(short vbucketID, long vbucketUUID, long sequenceNumber, String bucket) {
-        this.vbucketID = vbucketID;
-        this.vbucketUUID = vbucketUUID;
-        this.sequenceNumber = sequenceNumber;
-        this.bucket = bucket;
+  public MutationToken(short vbucketID, long vbucketUUID, long sequenceNumber, String bucket) {
+    this.vbucketID = vbucketID;
+    this.vbucketUUID = vbucketUUID;
+    this.sequenceNumber = sequenceNumber;
+    this.bucket = bucket;
+  }
+
+  public long vbucketUUID() {
+    return vbucketUUID;
+  }
+
+  public long sequenceNumber() {
+    return sequenceNumber;
+  }
+
+  public short vbucketID() {
+    return vbucketID;
+  }
+
+  public String bucket() {
+    return bucket;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
     }
-
-    public long vbucketUUID() {
-        return vbucketUUID;
+    if (o == null || getClass() != o.getClass()) {
+      return false;
     }
+    MutationToken that = (MutationToken) o;
+    return vbucketID == that.vbucketID &&
+        vbucketUUID == that.vbucketUUID &&
+        sequenceNumber == that.sequenceNumber &&
+        Objects.equals(bucket, that.bucket);
+  }
 
-    public long sequenceNumber() {
-        return sequenceNumber;
-    }
+  @Override
+  public int hashCode() {
+    return Objects.hash(vbucketID, vbucketUUID, sequenceNumber, bucket);
+  }
 
-    public short vbucketID() {
-        return vbucketID;
-    }
-
-    public String bucket() {
-        return bucket;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        MutationToken that = (MutationToken) o;
-
-        if (vbucketID != that.vbucketID) return false;
-        if (vbucketUUID != that.vbucketUUID) return false;
-        if (sequenceNumber != that.sequenceNumber) return false;
-        return bucket != null ? bucket.equals(that.bucket) : that.bucket == null;
-
-    }
-
-    @Override
-    public int hashCode() {
-        int result = (int) (vbucketID ^ (vbucketID >>> 32));
-        result = 31 * result + (int) (vbucketUUID ^ (vbucketUUID >>> 32));
-        result = 31 * result + (int) (sequenceNumber ^ (sequenceNumber >>> 32));
-        result = 31 * result + (bucket != null ? bucket.hashCode() : 0);
-        return result;
-    }
-
-    @Override
-    public String toString() {
-        final StringBuilder sb = new StringBuilder("mt{");
-        sb.append("vbID=").append(vbucketID);
-        sb.append(", vbUUID=").append(vbucketUUID);
-        sb.append(", seqno=").append(sequenceNumber);
-        sb.append(", bucket=").append(bucket);
-        sb.append('}');
-        return sb.toString();
-    }
+  @Override
+  public String toString() {
+    return "mt{" +
+        "vbID=" + vbucketID +
+        ", vbUUID=" + vbucketUUID +
+        ", seqno=" + sequenceNumber +
+        ", bucket=" + bucket +
+        '}';
+  }
 }
