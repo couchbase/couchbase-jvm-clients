a85d6b6f0f4e09dd4f175e86cc01626e8c9373c1
Michael Nitschinger <michael@nitschinger.at>
2020-02-10 07:20:49 +0100

    Add subdoc spec javadocs and polish mutate in helper.
    This changeset updates and polishes the javadocs for the mutate
    and lookup specs. It also slightly tweaks the mutate in utils
    where a more efficient byte array encoding strategy is used with
    netty to avoid memory copies where possible.
    
    Additionally, test cases have been added for the three common cases
    the util class will see.
    
    Change-Id: I2854e37ac1d4079c749bc866a126574835fec5f9
    Reviewed-on: http://review.couchbase.org/121936
    Tested-by: Build Bot <build@couchbase.com>
    Reviewed-by: Graham Pople <grahampople@gmail.com>

--- java-client/src/main/java/com/couchbase/client/java/kv/MutateInUtil.java
+++ java-client/src/main/java/com/couchbase/client/java/kv/MutateInUtil.java
@@ -17,6 +17,7 @@ package com.couchbase.client.java.kv;
 
 import com.couchbase.client.core.annotation.Stability;
 import com.couchbase.client.core.deps.io.netty.buffer.ByteBuf;
+import com.couchbase.client.core.deps.io.netty.buffer.ByteBufUtil;
 import com.couchbase.client.core.deps.io.netty.buffer.PooledByteBufAllocator;
 import com.couchbase.client.core.deps.io.netty.buffer.UnpooledByteBufAllocator;
 import com.couchbase.client.java.codec.JsonSerializer;
@@ -25,19 +26,30 @@ import java.util.Iterator;
 import java.util.List;
 
 /**
- * Internal utility methods for MutateIn.
+ * Internal utility methods for the MutateIn subdoc operation.
  */
 @Stability.Internal
 public class MutateInUtil {
+
   private MutateInUtil() {}
 
-  static byte[] convertDocsToBytes(List<?> docs, JsonSerializer serializer) {
+  /**
+   * Converts a list of objects (subdoc partials) into their low-level representation.
+   * <p>
+   * Per KV protocol each individual value is separated by a ",", which is not valid JSON on
+   * the wire but the server will split it apart and apply each one of them individually.
+   *
+   * @param docs the docs to serialize.
+   * @param serializer the serializer to use for JSON.
+   * @return the serialized and converted byte array.
+   */
+  static byte[] convertDocsToBytes(final List<?> docs, final JsonSerializer serializer) {
     if (docs.size() == 1) {
       return serializer.serialize(docs.get(0));
     } else {
-      ByteBuf bytes = UnpooledByteBufAllocator.DEFAULT.buffer();
+      final ByteBuf bytes = UnpooledByteBufAllocator.DEFAULT.heapBuffer();
       try {
-        Iterator<?> it = docs.iterator();
+        final Iterator<?> it = docs.iterator();
         while (it.hasNext()) {
           Object d = it.next();
           bytes.writeBytes(serializer.serialize(d));
@@ -45,13 +57,11 @@ public class MutateInUtil {
             bytes.writeByte(',');
           }
         }
-        byte[] content = new byte[bytes.readableBytes()];
-        bytes.readBytes(content);
-        return content;
-      }
-      finally {
+        return ByteBufUtil.getBytes(bytes);
+      } finally {
         bytes.release();
       }
     }
   }
+
 }
