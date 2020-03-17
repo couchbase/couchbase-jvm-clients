121321e6409d4e09552bfc8e67acda2d718e81b9
Michael Nitschinger <michael@nitschinger.at>
2020-03-17 18:09:20 +0100

    JVMCBC-830: More convenience overloads for certificates
    Based on forum feedback it has been identified that the current
    overloads are not as convenient, especially when converting from
    SDK2 to SDK3.
    
    This changeset adds more overloads to initialize both the
    SecurityConfig and the CertificateAuthenticator directly from
    a KeyStore/TrustStore and it also improves the javadocs all
    around a bit.
    
    Change-Id: Iedf0d3b412b79960ec7ece95a8edd0ed57380252
    Reviewed-on: http://review.couchbase.org/123849
    Tested-by: Build Bot <build@couchbase.com>
    Reviewed-by: Graham Pople <grahampople@gmail.com>
    Reviewed-on: http://review.couchbase.org/c/couchbase-jvm-clients/+/124718
    Tested-by: Michael Nitschinger <michael.nitschinger@couchbase.com>
    Reviewed-by: Michael Nitschinger <michael.nitschinger@couchbase.com>

--- core-io/src/main/java/com/couchbase/client/core/env/CertificateAuthenticator.java
+++ core-io/src/main/java/com/couchbase/client/core/env/CertificateAuthenticator.java
@@ -20,15 +20,20 @@ import com.couchbase.client.core.deps.io.netty.handler.ssl.SslContextBuilder;
 import com.couchbase.client.core.error.InvalidArgumentException;
 
 import javax.net.ssl.KeyManagerFactory;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.security.KeyStore;
 import java.security.PrivateKey;
 import java.security.cert.X509Certificate;
 import java.util.List;
+import java.util.Optional;
 import java.util.function.Supplier;
 
 import static com.couchbase.client.core.util.Validators.notNull;
+import static com.couchbase.client.core.util.Validators.notNullOrEmpty;
 
 /**
- * Performs authentication through a client certificate.
+ * Performs authentication through a client certificate instead of supplying username and password.
  */
 public class CertificateAuthenticator implements Authenticator {
 
@@ -37,14 +42,76 @@ public class CertificateAuthenticator implements Authenticator {
   private final List<X509Certificate> keyCertChain;
   private final Supplier<KeyManagerFactory> keyManagerFactory;
 
+  /**
+   * Creates a new {@link CertificateAuthenticator} from a key store path.
+   *
+   * @param keyStorePath the file path to the keystore.
+   * @param keyStorePassword the password for the keystore.
+   * @param keyStoreType the type of the key store. If empty, the {@link KeyStore#getDefaultType()} will be used.
+   * @return the created {@link CertificateAuthenticator}.
+   */
+  public static CertificateAuthenticator fromKeyStore(final Path keyStorePath, final String keyStorePassword,
+                                                      final Optional<String> keyStoreType) {
+    notNull(keyStorePath, "KeyStorePath");
+    notNull(keyStoreType, "KeyStoreType");
+
+    try {
+      final KeyStore store = KeyStore.getInstance(keyStoreType.orElse(KeyStore.getDefaultType()));
+      store.load(
+        Files.newInputStream(keyStorePath),
+        keyStorePassword != null ? keyStorePassword.toCharArray() : null
+      );
+      return fromKeyStore(store, keyStorePassword);
+    } catch (Exception ex) {
+      throw InvalidArgumentException.fromMessage("Could not initialize KeyStore from Path", ex);
+    }
+  }
+
+  /**
+   * Creates a new {@link CertificateAuthenticator} from a key store.
+   *
+   * @param keyStore the key store to load the certificate from.
+   * @param keyStorePassword the password for the key store.
+   * @return the created {@link CertificateAuthenticator}.
+   */
+  public static CertificateAuthenticator fromKeyStore(final KeyStore keyStore, final String keyStorePassword) {
+    notNull(keyStore, "KeyStore");
+
+    try {
+      final KeyManagerFactory kmf = KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm());
+      kmf.init(
+        keyStore,
+        keyStorePassword != null ? keyStorePassword.toCharArray() : null
+      );
+      return fromKeyManagerFactory(() -> kmf);
+    } catch (Exception ex) {
+      throw InvalidArgumentException.fromMessage("Could not initialize KeyManagerFactory with KeyStore", ex);
+    }
+  }
+
+  /**
+   * Creates a new {@link CertificateAuthenticator} from a {@link KeyManagerFactory}.
+   *
+   * @param keyManagerFactory the key manager factory in a supplier that should be used.
+   * @return the created {@link CertificateAuthenticator}.
+   */
   public static CertificateAuthenticator fromKeyManagerFactory(final Supplier<KeyManagerFactory> keyManagerFactory) {
     notNull(keyManagerFactory, "KeyManagerFactory");
     return new CertificateAuthenticator(null, null, null, keyManagerFactory);
   }
 
+  /**
+   * Creates a new {@link CertificateAuthenticator} directly from a key and certificate chain.
+   *
+   * @param key the private key to authenticate.
+   * @param keyPassword the password for to use.
+   * @param keyCertChain the key certificate chain to use.
+   * @return the created {@link CertificateAuthenticator}.
+   */
   public static CertificateAuthenticator fromKey(final PrivateKey key, final String keyPassword,
                                                  final List<X509Certificate> keyCertChain) {
     notNull(key, "PrivateKey");
+    notNullOrEmpty(keyCertChain, "KeyCertChain");
     return new CertificateAuthenticator(key, keyPassword, keyCertChain, null);
   }
 
@@ -80,9 +147,9 @@ public class CertificateAuthenticator implements Authenticator {
   public String toString() {
     return "CertificateAuthenticator{" +
       "key=" + key +
-      ", keyPassword='" + keyPassword + '\'' +
       ", keyCertChain=" + keyCertChain +
       ", keyManagerFactory=" + keyManagerFactory +
       '}';
   }
+
 }
--- core-io/src/main/java/com/couchbase/client/core/env/SecurityConfig.java
+++ core-io/src/main/java/com/couchbase/client/core/env/SecurityConfig.java
@@ -22,12 +22,11 @@ import com.couchbase.client.core.error.InvalidArgumentException;
 import javax.net.ssl.KeyManagerFactory;
 import javax.net.ssl.TrustManagerFactory;
 import java.io.ByteArrayInputStream;
-import java.io.File;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.security.PrivateKey;
+import java.security.KeyStore;
 import java.security.cert.CertificateException;
 import java.security.cert.CertificateFactory;
 import java.security.cert.X509Certificate;
@@ -35,12 +34,25 @@ import java.util.Collections;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import static com.couchbase.client.core.util.Validators.notNull;
+import static com.couchbase.client.core.util.Validators.notNullOrEmpty;
 
+/**
+ * The {@link SecurityConfig} allows to enable transport encryption between the client and the servers.
+ */
 public class SecurityConfig {
 
+  /**
+   * By default, TLS is disabled.
+   */
+  private static final boolean DEFAULT_TLS_ENABLED = false;
+
+  /**
+   * By default, netty native tls (OpenSSL) is enabled for better performance.
+   */
   private static final boolean DEFAULT_NATIVE_TLS_ENABLED = true;
 
   private final boolean nativeTlsEnabled;
@@ -48,31 +60,96 @@ public class SecurityConfig {
   private final List<X509Certificate> trustCertificates;
   private final TrustManagerFactory trustManagerFactory;
 
-
+  /**
+   * Creates a builder to customize the {@link SecurityConfig} configuration.
+   *
+   * @return the builder to customize.
+   */
   public static Builder builder() {
     return new Builder();
   }
 
+  /**
+   * Creates a {@link SecurityConfig} with the default configuration.
+   *
+   * @return the default security config.
+   */
   public static SecurityConfig create() {
     return new SecurityConfig(builder());
   }
 
+  /**
+   * Enables TLS for all client/server communication (disabled by default).
+   *
+   * @param tlsEnabled true if enabled, false otherwise.
+   * @return this {@link Builder} for chaining purposes.
+   */
   public static Builder enableTls(boolean tlsEnabled) {
     return builder().enableTls(tlsEnabled);
   }
 
+  /**
+   * Enables/disables native TLS (enabled by default).
+   *
+   * @param nativeTlsEnabled true if it should be enabled, false otherwise.
+   * @return this {@link Builder} for chaining purposes.
+   */
   public static Builder enableNativeTls(boolean nativeTlsEnabled) {
     return builder().enableNativeTls(nativeTlsEnabled);
   }
 
+  /**
+   * Loads the given list of X.509 certificates into the trust store.
+   *
+   * @param certificates the list of certificates to load.
+   * @return this {@link Builder} for chaining purposes.
+   */
   public static Builder trustCertificates(final List<X509Certificate> certificates) {
     return builder().trustCertificates(certificates);
   }
 
+  /**
+   * Loads a X.509 trust certificate from the given path and uses it.
+   *
+   * @param certificatePath the path to load the certificate from.
+   * @return this {@link Builder} for chaining purposes.
+   */
   public static Builder trustCertificate(final Path certificatePath) {
     return builder().trustCertificate(certificatePath);
   }
 
+  /**
+   * Initializes the {@link TrustManagerFactory} with the given trust store.
+   *
+   * @param trustStore the loaded trust store to use.
+   * @return this {@link Builder} for chaining purposes.
+   */
+  public static Builder trustStore(final KeyStore trustStore) {
+    return builder().trustStore(trustStore);
+  }
+
+  /**
+   * Loads a trust store from a file path and password and initializes the {@link TrustManagerFactory}.
+   *
+   * @param trustStorePath the path to the truststore.
+   * @param trustStorePassword the password (can be null if not password protected).
+   * @param trustStoreType the type of the trust store. If empty, the {@link KeyStore#getDefaultType()} will be used.
+   * @return this {@link Builder} for chaining purposes.
+   */
+  public static Builder trustStore(final Path trustStorePath, final String trustStorePassword,
+                                   final Optional<String> trustStoreType) {
+    return builder().trustStore(trustStorePath, trustStorePassword, trustStoreType);
+  }
+
+  /**
+   * Allows to provide a trust manager factory directly for maximum flexibility.
+   * <p>
+   * While providing the most flexibility, most users will find the other overloads more convenient, like passing
+   * in a {@link #trustStore(KeyStore)} directly or via filepath {@link #trustStore(Path, String, Optional)}.
+   *
+   * @param trustManagerFactory the trust manager factory to use.
+   * @return this {@link Builder} for chaining purposes.
+   */
   public static Builder trustManagerFactory(final TrustManagerFactory trustManagerFactory) {
     return builder().trustManagerFactory(trustManagerFactory);
   }
@@ -96,18 +173,38 @@ public class SecurityConfig {
     }
   }
 
+  /**
+   * True if TLS is enabled, false otherwise.
+   *
+   * @return a boolean if tls/transport encryption is enabled.
+   */
   public boolean tlsEnabled() {
     return tlsEnabled;
   }
 
+  /**
+   * The list of trust certificates that should be used, if present.
+   *
+   * @return the list of certificates.
+   */
   public List<X509Certificate> trustCertificates() {
     return trustCertificates;
   }
 
+  /**
+   * The currently configured trust manager factory, if present.
+   *
+   * @return the trust manager factory.
+   */
   public TrustManagerFactory trustManagerFactory() {
     return trustManagerFactory;
   }
 
+  /**
+   * Returns whether native TLS is enabled.
+   *
+   * @return true if enabled, false otherwise.
+   */
   public boolean nativeTlsEnabled() {
     return nativeTlsEnabled;
   }
@@ -117,7 +214,7 @@ public class SecurityConfig {
    */
   @Stability.Volatile
   Map<String, Object> exportAsMap() {
-    Map<String, Object> export = new LinkedHashMap<>();
+    final Map<String, Object> export = new LinkedHashMap<>();
     export.put("tlsEnabled", tlsEnabled);
     export.put("nativeTlsEnabled", nativeTlsEnabled);
     export.put("hasTrustCertificates", trustCertificates != null && !trustCertificates.isEmpty());
@@ -125,46 +222,139 @@ public class SecurityConfig {
     return export;
   }
 
+  /**
+   * This builder allows to customize the default security configuration.
+   */
   public static class Builder {
 
-    private boolean tlsEnabled = false;
+    private boolean tlsEnabled = DEFAULT_TLS_ENABLED;
     private boolean nativeTlsEnabled = DEFAULT_NATIVE_TLS_ENABLED;
     private List<X509Certificate> trustCertificates = null;
     private TrustManagerFactory trustManagerFactory = null;
 
+    /**
+     * Builds the {@link SecurityConfig} out of this builder.
+     *
+     * @return the built security config.
+     */
     public SecurityConfig build() {
       return new SecurityConfig(this);
     }
 
+    /**
+     * Enables TLS for all client/server communication (disabled by default).
+     *
+     * @param tlsEnabled true if enabled, false otherwise.
+     * @return this {@link Builder} for chaining purposes.
+     */
     public Builder enableTls(boolean tlsEnabled) {
       this.tlsEnabled = tlsEnabled;
       return this;
     }
 
+    /**
+     * Enables/disables native TLS (enabled by default).
+     *
+     * @param nativeTlsEnabled true if it should be enabled, false otherwise.
+     * @return this {@link Builder} for chaining purposes.
+     */
     public Builder enableNativeTls(boolean nativeTlsEnabled) {
       this.nativeTlsEnabled = nativeTlsEnabled;
       return this;
     }
 
+    /**
+     * Loads the given list of X.509 certificates into the trust store.
+     *
+     * @param certificates the list of certificates to load.
+     * @return this {@link Builder} for chaining purposes.
+     */
     public Builder trustCertificates(final List<X509Certificate> certificates) {
-      this.trustCertificates = certificates;
+      this.trustCertificates = notNullOrEmpty(certificates, "X509 Certificates");
       return this;
     }
 
+    /**
+     * Loads a X.509 trust certificate from the given path and uses it.
+     *
+     * @param certificatePath the path to load the certificate from.
+     * @return this {@link Builder} for chaining purposes.
+     */
     public Builder trustCertificate(final Path certificatePath) {
+      notNull(certificatePath, "CertificatePath");
+
       final StringBuilder contentBuilder = new StringBuilder();
       try {
         Files.lines(certificatePath, StandardCharsets.UTF_8).forEach(s -> contentBuilder.append(s).append("\n"));
       } catch (IOException ex) {
-        throw InvalidArgumentException.fromMessage("Could not read trust certificate from file \"" + certificatePath + "\"" , ex);
+        throw InvalidArgumentException.fromMessage(
+          "Could not read trust certificate from file \"" + certificatePath + "\"" ,
+          ex
+        );
       }
       return trustCertificates(decodeCertificates(Collections.singletonList(contentBuilder.toString())));
     }
 
+    /**
+     * Allows to provide a trust manager factory directly for maximum flexibility.
+     * <p>
+     * While providing the most flexibility, most users will find the other overloads more convenient, like passing
+     * in a {@link #trustStore(KeyStore)} directly or via filepath {@link #trustStore(Path, String, Optional)}.
+     *
+     * @param trustManagerFactory the trust manager factory to use.
+     * @return this {@link Builder} for chaining purposes.
+     */
     public Builder trustManagerFactory(final TrustManagerFactory trustManagerFactory) {
-      this.trustManagerFactory = trustManagerFactory;
+      this.trustManagerFactory = notNull(trustManagerFactory, "TrustManagerFactory");
       return this;
     }
+
+    /**
+     * Initializes the {@link TrustManagerFactory} with the given trust store.
+     *
+     * @param trustStore the loaded trust store to use.
+     * @return this {@link Builder} for chaining purposes.
+     */
+    public Builder trustStore(final KeyStore trustStore) {
+      notNull(trustStore, "TrustStore");
+
+      try {
+        final TrustManagerFactory tmf = TrustManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm());
+        tmf.init(trustStore);
+        return trustManagerFactory(tmf);
+      } catch (Exception ex) {
+        throw InvalidArgumentException.fromMessage(
+          "Could not initialize TrustManagerFactory from TrustStore",
+          ex
+        );
+      }
+    }
+
+    /**
+     * Loads a trust store from a file path and password and initializes the {@link TrustManagerFactory}.
+     *
+     * @param trustStorePath the path to the truststore.
+     * @param trustStorePassword the password (can be null if not password protected).
+     * @param trustStoreType the type of the trust store. If empty, the {@link KeyStore#getDefaultType()} will be used.
+     * @return this {@link Builder} for chaining purposes.
+     */
+    public Builder trustStore(final Path trustStorePath, final String trustStorePassword,
+                              final Optional<String> trustStoreType) {
+      notNull(trustStorePath, "TrustStorePath");
+      notNull(trustStoreType, "TrustStoreType");
+
+      try {
+        final KeyStore store = KeyStore.getInstance(trustStoreType.orElse(KeyStore.getDefaultType()));
+        store.load(
+          Files.newInputStream(trustStorePath),
+          trustStorePassword != null ? trustStorePassword.toCharArray() : null
+        );
+        return trustStore(store);
+      } catch (Exception ex) {
+        throw InvalidArgumentException.fromMessage("Could not initialize TrustStore", ex);
+      }
+    }
+
   }
 
   /**
@@ -174,6 +364,8 @@ public class SecurityConfig {
    * @return the decoded certs in x.509 format.
    */
   public static List<X509Certificate> decodeCertificates(final List<String> certificates) {
+    notNull(certificates, "Certificates");
+
     final CertificateFactory cf;
     try {
       cf = CertificateFactory.getInstance("X.509");
@@ -191,4 +383,5 @@ public class SecurityConfig {
       }
     }).collect(Collectors.toList());
   }
+
 }
