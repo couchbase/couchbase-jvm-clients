c8c612bfd0f660a013bfec158eaa46e74bffb7e3
David Nault <david.nault@couchbase.com>
2020-06-23 12:55:33 -0700

    JCBC-1645 Expiry durations longer than 30 days are interpreted incorrectly
    Motivation
    ----------
    Couchbase Server interprets the "expiry" field different depending
    on whether it's less than 30 days. If it's less than 30 days, it's
    interpreted as a relative offset in seconds from the current time.
    Otherwise it's interpreted as an absolute timestamp in UNIX epoch seconds.
    
    Prior to this change, the SDK would pass the number of seconds in the
    duration directly to the server, causing documents to expire
    prematurely for durations longer than 30 days.
    
    Modifications
    -------------
    When generating the request, convert expiry durations longer
    than 30 days into absolute timestamps. (Unless they're longer
    than 50 years, in which case treat them as absolute for
    bug-compatibility with previous 3.0.x versions.)
    
    Recompute the expiry every time a request is executed. This way
    the effective expiry duration is relative to the time the request
    is executed, not the time the options block was created.
    
    Change-Id: I5d7090e789af45d83a61cd1cdff99d8270d7948a
    Reviewed-on: http://review.couchbase.org/c/couchbase-jvm-clients/+/131215
    Tested-by: Build Bot <build@couchbase.com>
    Reviewed-by: Michael Reiche <michael.reiche@couchbase.com>
    (cherry picked from commit 93e610b5c810fb26d8f9b0ef4be7aca09786d0d4)
    Reviewed-on: http://review.couchbase.org/c/couchbase-jvm-clients/+/131951
    Reviewed-by: David Nault <david.nault@couchbase.com>

---
+++ core-io/src/main/java/com/couchbase/client/core/msg/kv/ExpiryUtils.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2020 Couchbase, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.couchbase.client.core.msg.kv;
+
+import com.couchbase.client.core.annotation.Stability;
+import com.couchbase.client.core.cnc.EventBus;
+import com.couchbase.client.core.cnc.events.request.SuspiciousExpiryDurationEvent;
+
+import java.time.Duration;
+
+import static java.util.concurrent.TimeUnit.DAYS;
+
+@Stability.Internal
+public class ExpiryUtils {
+  private ExpiryUtils() {
+    throw new AssertionError("not instantiable");
+  }
+
+  private static final int RELATIVE_EXPIRY_CUTOFF_SECONDS = (int) DAYS.toSeconds(30);
+  private static final int WORKAROUND_EXPIRY_CUTOFF_SECONDS = (int) DAYS.toSeconds(365) * 50;
+
+  /**
+   * If the duration is less than 30 days, returns the number of seconds
+   * in the duration, otherwise returns the current time in Unix epoch seconds
+   * plus the number of seconds in the duration.
+   */
+  public static int getAdjustedExpirySeconds(Duration duration, EventBus eventBus) {
+    long seconds = duration.getSeconds();
+    if (seconds < RELATIVE_EXPIRY_CUTOFF_SECONDS) {
+      return (int) seconds;
+    }
+
+    // Some users may have worked around JCBC-1645 by stuffing the absolute timestamp
+    // into the duration. Accommodate them by passing very large values through
+    // unmodified.
+    if (seconds > WORKAROUND_EXPIRY_CUTOFF_SECONDS) {
+      eventBus.publish(new SuspiciousExpiryDurationEvent(duration));
+      return (int) Math.min(Integer.MAX_VALUE, seconds);
+    }
+
+    long absolute = (System.currentTimeMillis() / 1000) + seconds;
+
+    // Prevent overflow due to Year 2038 problem.
+    // Not ideal, but at least the documents won't expire immediately.
+    return (int) Math.min(Integer.MAX_VALUE, absolute);
+  }
+}
