f95bcb49bc8c735c3f69847fb6d8178c7802ba5a
Michael Nitschinger <michael@nitschinger.at>
2020-09-02 13:19:43 +0200

    JCBC-1695: Add disableScoring search option
    This introduces a new option which allows to bypass the default
    scoring to speed up querying if TF/IDF scoring is not needed.
    
    Change-Id: I0bf79aedf786b2c9025699e11c399073a638e944
    Reviewed-on: http://review.couchbase.org/c/couchbase-jvm-clients/+/135437
    Tested-by: Build Bot <build@couchbase.com>
    Reviewed-by: David Nault <david.nault@couchbase.com>
    Reviewed-on: http://review.couchbase.org/c/couchbase-jvm-clients/+/137235
    Tested-by: Michael Nitschinger <michael.nitschinger@couchbase.com>
    Reviewed-by: Michael Nitschinger <michael.nitschinger@couchbase.com>

---
+++ java-client/src/test/java/com/couchbase/client/java/search/SearchOptionsTest.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) 2020 Couchbase, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.couchbase.client.java.search;
+
+import com.couchbase.client.java.json.JsonObject;
+import org.junit.jupiter.api.Test;
+
+import static com.couchbase.client.java.search.SearchOptions.searchOptions;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+/**
+ * Verifies the functionality of the {@link SearchOptions}.
+ */
+class SearchOptionsTest {
+
+  /**
+   * Makes sure that (only) when scoring is disabled, it shows up in the resulting query.
+   */
+  @Test
+  void allowToDisableScoring() {
+    JsonObject output = JsonObject.create();
+    searchOptions().disableScoring(true).build().injectParams("idx", output);
+    assertEquals(output.getString("score"), "none");
+
+    output = JsonObject.create();
+    searchOptions().disableScoring(false).build().injectParams("idx", output);
+    assertFalse(output.containsKey("score"));
+
+    output = JsonObject.create();
+    searchOptions().build().injectParams("idx", output);
+    assertFalse(output.containsKey("score"));
+  }
+
+}
