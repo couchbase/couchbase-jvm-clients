beabcc6bab300c5a91f4fa917c0207ba58fb4abe
Graham Pople <grahampople@gmail.com>
2020-09-11 15:24:32 +0100

    SCBC-233: Add support for search geopolygon queries
    This changeset adds a new search query method which allows specifying
    a list of coordinates that build a polygon which are then sent to
    the search service.
    
    Change-Id: Ib0ea86996223a7fc8b1cf41568c0148a960305f0
    Reviewed-on: http://review.couchbase.org/c/couchbase-jvm-clients/+/136048
    Tested-by: Build Bot <build@couchbase.com>
    Reviewed-by: David Nault <david.nault@couchbase.com>
    Reviewed-by: Michael Nitschinger <michael.nitschinger@couchbase.com>

---
+++ scala-client/src/main/scala/com/couchbase/client/scala/util/Coordinate.scala
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) 2020 Couchbase, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.couchbase.client.scala.util
+
+/** A coordinate is a tuple of a latitude and a longitude. */
+object Coordinate {
+
+  /**
+    * Creates a new [[Coordinate]] with a longitude and a latitude.
+    *
+    * @param lon the longitude of the coordinate.
+    * @param lat the latitude of the coordinate.
+    * @return a new Coordinate.
+    */
+  def ofLonLat(lon: Double, lat: Double) = new Coordinate(lon, lat)
+}
+
+case class Coordinate private (
+    /**
+      * The longitude of this coordinate.
+      */
+    lon: Double,
+    /**
+      * The latitude of this coordinate.
+      */
+    lat: Double
+)
