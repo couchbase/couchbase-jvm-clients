d9768737edffa0a054b0845c437fd97a61050ff3
Michael Nitschinger <michael@nitschinger.at>
2020-12-03 14:47:04 +0100

    JCBC-1728: Fix collection manager error parsing
    Recent server changes in CC changed the error output format so
    this change modifies the error string parsing to map them to the
    correct exceptions again.
    
    Change-Id: I5dea7a76fa7229514673513a45db7d10b89636b5
    Reviewed-on: http://review.couchbase.org/c/couchbase-jvm-clients/+/141422
    Tested-by: Build Bot <build@couchbase.com>
    Reviewed-by: Will Broadbelt <william.broadbelt@couchbase.com>
    Reviewed-by: Michael Reiche <michael.reiche@couchbase.com>

--- java-client/src/main/java/com/couchbase/client/java/manager/collection/AsyncCollectionManager.java
+++ java-client/src/main/java/com/couchbase/client/java/manager/collection/AsyncCollectionManager.java
@@ -272,7 +272,7 @@ public class AsyncCollectionManager extends ManagerSupport {
         throw FeatureNotAvailableException.collections();
       }
 
-      if (error.contains("Scope with this name is not found")) {
+      if (error.contains("Scope with this name is not found") || error.contains("scope_not_found")) {
         throw ScopeNotFoundException.forScope(scopeName);
       }
 
@@ -282,15 +282,24 @@ public class AsyncCollectionManager extends ManagerSupport {
     }
 
     if (response.status() == ResponseStatus.INVALID_ARGS) {
-      if (error.contains("Scope with this name already exists")) {
+      if (error.contains("Scope with this name already exists")
+        || error.matches(".*Scope with name .+ already exists.*")) {
         throw ScopeExistsException.forScope(scopeName);
       }
-      if (error.contains("Collection with this name already exists")) {
+      if (error.contains("scope_not_found")) {
+        throw ScopeNotFoundException.forScope(scopeName);
+      }
+      if (error.contains("Collection with this name already exists")
+        || error.matches(".*Collection with name .+ already exists.*")) {
         throw CollectionExistsException.forCollection(collectionName);
       }
       if (error.contains("Not allowed on this version of cluster")) {
         // This happens on 6.5 if collections dev preview is not enabled
         throw FeatureNotAvailableException.collections();
+      }
+      if (error.contains("Collection with this name is not found")
+        || error.contains("collection_not_found")) {
+        throw CollectionNotFoundException.forCollection(collectionName);
       }
     }
 
