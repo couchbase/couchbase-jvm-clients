8801009de132bb7338c9a6bf592fe2cd93501ec2
David Nault <david.nault@couchbase.com>
2021-02-22 18:08:02 -0800

    Maven scaffolding for Kotlin client
    Change-Id: I00eef40c9b7597cf5177f071b239a4833347ca6b
    Reviewed-on: http://review.couchbase.org/c/couchbase-jvm-clients/+/146772
    Tested-by: Build Bot <build@couchbase.com>
    Reviewed-by: Michael Nitschinger <michael.nitschinger@couchbase.com>

---
+++ kotlin-client/src/main/kotlin/com/couchbase/client/kotlin/internal/JavadocPlaceholder.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright 2021 Couchbase, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.couchbase.client.kotlin.internal;
+
+/**
+ * Quick and dirty way to generate the javadocs.jar required by Maven Central.
+ * Please disregard.
+ * <p>
+ * TODO: Use Dokka to generate Javadocs from Kotlin source
+ * once they have a working distribution on Maven Central.
+ *
+ * @deprecated This is not the class you're looking for.
+ */
+@Deprecated
+class JavadocPlaceholder {
+}
