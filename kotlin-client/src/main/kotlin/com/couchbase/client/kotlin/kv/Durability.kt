349f377f05614e1da7cebca64bf0857cbc4b7a21
David Nault <david.nault@couchbase.com>
2021-02-27 10:03:42 -0800

    KCBC-12 Durability options
    Change-Id: I51d1fc7ce2f09e3818157d2bdcc9f12e2d72b239
    Reviewed-on: http://review.couchbase.org/c/couchbase-jvm-clients/+/147242
    Tested-by: Build Bot <build@couchbase.com>
    Reviewed-by: Michael Nitschinger <michael.nitschinger@couchbase.com>

---
+++ kotlin-client/src/main/kotlin/com/couchbase/client/kotlin/kv/Durability.kt
@@ -0,0 +1,89 @@
+package com.couchbase.client.kotlin.kv
+
+import com.couchbase.client.core.msg.kv.DurabilityLevel
+import com.couchbase.client.core.msg.kv.DurabilityLevel.MAJORITY
+import com.couchbase.client.core.msg.kv.DurabilityLevel.MAJORITY_AND_PERSIST_TO_ACTIVE
+import com.couchbase.client.core.msg.kv.DurabilityLevel.PERSIST_TO_MAJORITY
+import com.couchbase.client.core.service.kv.Observe.ObservePersistTo
+import com.couchbase.client.core.service.kv.Observe.ObserveReplicateTo
+
+/**
+ * Specifies the durability requirements for a mutation.
+ */
+public sealed class Durability {
+
+    public object Disabled : Durability() {
+        override fun toString(): String = "Disabled"
+    }
+
+    public data class Synchronous internal constructor(
+        val level: DurabilityLevel,
+    ) : Durability()
+
+    public data class ClientVerified internal constructor(
+        val persistTo: PersistTo,
+        val replicateTo: ReplicateTo,
+    ) : Durability()
+
+    public companion object {
+        /**
+         * The SDK will report success as soon as the node hosting the
+         * active partition has the mutation in memory (but not necessarily
+         * persisted to disk).
+         */
+        public fun disabled(): Durability = Disabled
+
+        /**
+         * The client will poll the server until the specified durability
+         * requirements are observed.
+         *
+         * This strategy is supported by all Couchbase Server versions,
+         * but has drawbacks. When using Couchbase Server 6.5 or later,
+         * the other durability options may be preferable.
+         */
+        public fun clientVerified(persistTo: PersistTo, replicateTo: ReplicateTo = ReplicateTo.NONE): Durability =
+            if (replicateTo == ReplicateTo.NONE && persistTo == PersistTo.NONE) Disabled
+            else ClientVerified(persistTo, replicateTo)
+
+        /**
+         * The mutation must be replicated to (that is, held in the memory
+         * allocated to the bucket on) a majority of the Data Service nodes.
+         *
+         * Requires Couchbase Server 6.5 or later.
+         */
+        public fun majority(): Durability = Synchronous(MAJORITY)
+
+        /**
+         * The mutation must be persisted to a majority of the Data Service nodes.
+         *
+         * Requires Couchbase Server 6.5 or later.
+         */
+        public fun persistToMajority(): Durability = Synchronous(PERSIST_TO_MAJORITY)
+
+        /**
+         * The mutation must be replicated to a majority of the Data Service nodes.
+         *
+         * Additionally, it must be persisted (that is, written and synchronised to disk)
+         * on the node hosting the active partition (vBucket) for the data.
+         *
+         * Requires Couchbase Server 6.5 or later.
+         */
+        public fun majorityAndPersistToActive(): Durability = Synchronous(MAJORITY_AND_PERSIST_TO_ACTIVE)
+    }
+}
+
+public enum class ReplicateTo(internal val coreHandle: ObserveReplicateTo) {
+    NONE(ObserveReplicateTo.NONE),
+    ONE(ObserveReplicateTo.ONE),
+    TWO(ObserveReplicateTo.TWO),
+    THREE(ObserveReplicateTo.THREE);
+}
+
+public enum class PersistTo(internal val coreHandle: ObservePersistTo) {
+    NONE(ObservePersistTo.NONE),
+    ACTIVE(ObservePersistTo.ACTIVE),
+    ONE(ObservePersistTo.ONE),
+    TWO(ObservePersistTo.TWO),
+    THREE(ObservePersistTo.THREE),
+    FOUR(ObservePersistTo.FOUR);
+}
