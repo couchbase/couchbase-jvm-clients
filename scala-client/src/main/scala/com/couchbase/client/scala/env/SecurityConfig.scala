b320eab646c7bd41051ad3373aa361a9275472f8
Michael Nitschinger <michael@nitschinger.at>
2021-08-30 15:43:07 +0200

    SCBC-329: Expose all builder methods for SecurityConfig
    Change-Id: I5426b7d76c79077348dccdaf862ba8b01ebd9009
    Reviewed-on: http://review.couchbase.org/c/couchbase-jvm-clients/+/160418
    Tested-by: Build Bot <build@couchbase.com>
    Reviewed-by: Michael Nitschinger <michael.nitschinger@couchbase.com>

--- scala-client/src/main/scala/com/couchbase/client/scala/env/SecurityConfig.scala
+++ scala-client/src/main/scala/com/couchbase/client/scala/env/SecurityConfig.scala
@@ -16,16 +16,26 @@
 package com.couchbase.client.scala.env
 
 import java.security.cert.X509Certificate
-
 import com.couchbase.client.core
+
+import java.nio.file.Path
+import java.security.KeyStore
+import java.util.Optional
 import javax.net.ssl.TrustManagerFactory
 import scala.jdk.CollectionConverters._
+
+case class TrustStoreFile(path: Path, password: String, storeType: Option[String])
 
 case class SecurityConfig(
     private[scala] val tlsEnabled: Option[Boolean] = None,
     private[scala] val nativeTlsEnabled: Option[Boolean] = None,
     private[scala] val trustCertificates: Option[Seq[X509Certificate]] = None,
-    private[scala] val trustManagerFactory: Option[TrustManagerFactory] = None
+    private[scala] val trustManagerFactory: Option[TrustManagerFactory] = None,
+    private[scala] val enableHostnameVerification: Option[Boolean] = None,
+    private[scala] val trustCertificate: Option[Path] = None,
+    private[scala] val ciphers: Option[Seq[String]] = None,
+    private[scala] val trustKeyStore: Option[KeyStore] = None,
+    private[scala] val trustStoreFile: Option[TrustStoreFile] = None
 ) {
   private[scala] def toCore: core.env.SecurityConfig.Builder = {
     val builder = new core.env.SecurityConfig.Builder
@@ -34,7 +44,13 @@ case class SecurityConfig(
     nativeTlsEnabled.foreach(v => builder.enableNativeTls(v))
     trustCertificates.foreach(t => builder.trustCertificates(t.asJava))
     trustManagerFactory.foreach(v => builder.trustManagerFactory(v))
-
+    enableHostnameVerification.foreach(v => builder.enableHostnameVerification(v))
+    trustCertificate.foreach(v => builder.trustCertificate(v))
+    ciphers.foreach(v => builder.ciphers(v.asJava))
+    trustKeyStore.foreach(v => builder.trustStore(v))
+    trustStoreFile.foreach(
+      v => builder.trustStore(v.path, v.password, Optional.ofNullable(v.storeType.orNull))
+    )
     builder
   }
 
@@ -46,11 +62,35 @@ case class SecurityConfig(
     copy(nativeTlsEnabled = Some(value))
   }
 
+  def enableHostnameVerification(value: Boolean): SecurityConfig = {
+    copy(enableHostnameVerification = Some(value))
+  }
+
   def trustCertificates(values: Seq[X509Certificate]): SecurityConfig = {
     copy(trustCertificates = Some(values))
   }
 
   def trustManagerFactory(value: TrustManagerFactory): SecurityConfig = {
     copy(trustManagerFactory = Some(value))
+  }
+
+  def trustCertificate(value: Path): SecurityConfig = {
+    copy(trustCertificate = Some(value))
+  }
+
+  def trustStore(value: KeyStore): SecurityConfig = {
+    copy(trustKeyStore = Some(value))
+  }
+
+  def trustStore(
+      trustStorePath: Path,
+      trustStorePassword: String,
+      trustStoreType: Option[String] = None
+  ): SecurityConfig = {
+    copy(trustStoreFile = Some(TrustStoreFile(trustStorePath, trustStorePassword, trustStoreType)))
+  }
+
+  def ciphers(value: Seq[String]): SecurityConfig = {
+    copy(ciphers = Some(value))
   }
 }
