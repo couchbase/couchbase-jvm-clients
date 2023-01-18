bbd872ab0079265ec2ad782668f966b2465df3ed
konstantin-umanets <konstantin.umanets@weigandt-consulting.com>
2023-01-18 16:12:04 +0400

    Complete CRUD operations test, extract constants

--- kotlin-examples/src/integrationTest/kotlin/com/couchbase/client/kotlin/examples/connection/SimpleOperationsTest.kt
+++ kotlin-examples/src/integrationTest/kotlin/com/couchbase/client/kotlin/examples/connection/SimpleOperationsTest.kt
@@ -1,21 +1,28 @@
 package com.couchbase.client.kotlin.examples.connection
 
+import com.couchbase.client.core.error.DocumentNotFoundException
 import com.couchbase.client.kotlin.Collection
 import com.couchbase.client.kotlin.examples.util.ConnectionUtils
+import com.couchbase.client.kotlin.examples.util.NEW_CONTENT
+import com.couchbase.client.kotlin.examples.util.TEST_BUCKET
+import com.couchbase.client.kotlin.examples.util.TEST_CONTENT
+import com.couchbase.client.kotlin.examples.util.TEST_ID
 import com.couchbase.client.kotlin.kv.GetResult
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertFalse
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Order
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 
 class SimpleOperationsTest {
 
     @Test
     @Order(1)
     fun `insert data to bucket`() {
-        ConnectionUtils.withBucket("travel-sample") { _, bucket ->
+        ConnectionUtils.withBucket(TEST_BUCKET) { _, bucket ->
             val collection: Collection = bucket.defaultCollection()
-            val mutationResult = collection.insert("airline_10", "Airline 10")
+            val mutationResult = collection.insert(TEST_ID, TEST_CONTENT)
             assertTrue(mutationResult.cas > 0)
         }
     }
@@ -23,13 +30,39 @@ class SimpleOperationsTest {
     @Test
     @Order(2)
     fun `get data from bucket`() {
-        ConnectionUtils.withBucket("travel-sample") { _, bucket ->
+        ConnectionUtils.withBucket(TEST_BUCKET) { _, bucket ->
             val collection: Collection = bucket.defaultCollection()
-            val result: GetResult = collection.get("airline_10")
+            val result: GetResult = collection.get(TEST_ID)
             val contentAsString = result.contentAs<String>()
-            assertTrue(contentAsString.contains("Airline 10"))
-            assertEquals("airline_10", result.id)
+            assertTrue(contentAsString.contains(TEST_CONTENT))
+            assertEquals(TEST_ID, result.id)
             println(contentAsString)
+        }
+    }
+
+    @Test
+    @Order(3)
+    fun `update data in bucket`() {
+        ConnectionUtils.withBucket(TEST_BUCKET) { _, bucket ->
+            val collection: Collection = bucket.defaultCollection()
+            collection.upsert(TEST_ID, NEW_CONTENT)
+            val result: GetResult = collection.get(TEST_ID)
+            val contentAsString = result.contentAs<String>()
+            assertFalse(contentAsString.contains(TEST_CONTENT))
+            assertTrue(contentAsString.contains(NEW_CONTENT))
+            println(contentAsString)
+        }
+    }
+
+    @Test
+    @Order(4)
+    fun `delete data from bucket`() {
+        ConnectionUtils.withBucket(TEST_BUCKET) { _, bucket ->
+            val collection: Collection = bucket.defaultCollection()
+            collection.remove(TEST_ID)
+            assertThrows<DocumentNotFoundException> {
+                collection.get(TEST_ID)
+            }
         }
     }
 
---
+++ kotlin-examples/src/integrationTest/kotlin/com/couchbase/client/kotlin/examples/util/Constants.kt
@@ -0,0 +1,6 @@
+package com.couchbase.client.kotlin.examples.util
+
+const val TEST_BUCKET = "travel-sample"
+const val TEST_ID = "airline_10"
+const val TEST_CONTENT = "Airline 10"
+const val NEW_CONTENT = "Airline 11"
