eff72dd434f819a1554ca758a79959f7bc197fe2
konstantin-umanets <konstantin.umanets@weigandt-consulting.com>
2023-01-27 17:43:36 +0400

    Fix after rebase

--- test-utils/src/main/java/com/couchbase/client/test/UnmanagedTestCluster.java
+++ test-utils/src/main/java/com/couchbase/client/test/UnmanagedTestCluster.java
@@ -34,6 +34,7 @@ import java.security.SecureRandom;
 import java.security.cert.CertificateFactory;
 import java.security.cert.X509Certificate;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
