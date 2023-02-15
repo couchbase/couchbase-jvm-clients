568acfdf6ae776037eb48fc2e24603fd4328268c
David Nault <david.nault@couchbase.com>
2023-02-15 11:37:01 -0800

    Add a "UsedBy" annotation for marking internal APIs used by other Couchbase projects.
    Change-Id: I28d0f3be7a9bee2f68b6525bb3bf38936e8f79f8
    Reviewed-on: https://review.couchbase.org/c/couchbase-jvm-clients/+/186814
    Tested-by: Build Bot <build@couchbase.com>
    Reviewed-by: Graham Pople <graham.pople@couchbase.com>

---
+++ core-io/src/main/java/com/couchbase/client/core/annotation/UsedBy.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2023 Couchbase, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.couchbase.client.core.annotation;
+
+import java.lang.annotation.Documented;
+
+/**
+ * Indicates the target element is not intended for public use,
+ * but some other Couchbase project depends on it.
+ * Changes to the API should be coordinated with the other project.
+ * <p>
+ * The target element should also be annotated with {@link Stability.Internal}.
+ * <p>
+ * Should be used sparingly, and only after deciding we don't want
+ * to promote the target element to the public API.
+ */
+@Documented
+@Stability.Internal // The annotation itself is internal API
+public @interface UsedBy {
+  Project value();
+
+  enum Project {
+    SPRING_DATA_COUCHBASE
+  }
+}
