couchbase/couchbase-jvm-clients

commit ee8584772945a73fdd9c3da090a5aec08602b733

JVMCBC-1218 Protostellar support for Rangescan
Refactor - push CoreRangeScan/CoreSampleScan and CoreScanOptions down
into RangeScanOrchestrator.

Change-Id: Ifef6e0c2f68e9e8982884b1d0b270c17f1e5767d
Reviewed-on: https://review.couchbase.org/c/couchbase-jvm-clients/+/186354
Tested-by: Build Bot <build@couchbase.com>
Reviewed-by: David Nault <david.nault@couchbase.com>

kotlin-client/src/main/kotlin/com/couchbase/client/kotlin/Collection.kt
@@ -20,7 +20,10 @@ import com.couchbase.client.core.Core
 import com.couchbase.client.core.CoreKeyspace
 import com.couchbase.client.core.annotation.SinceCouchbase
 import com.couchbase.client.core.api.kv.CoreAsyncResponse
+import com.couchbase.client.core.api.shared.CoreMutationState
+import com.couchbase.client.core.cnc.RequestSpan
 import com.couchbase.client.core.cnc.TracingIdentifiers
+import com.couchbase.client.core.endpoint.http.CoreCommonOptions
 import com.couchbase.client.core.env.TimeoutConfig
 import com.couchbase.client.core.error.CasMismatchException
 import com.couchbase.client.core.error.DefaultErrorUtil
@@ -30,8 +33,7 @@ import com.couchbase.client.core.error.DocumentUnretrievableException
 import com.couchbase.client.core.error.InvalidArgumentException
 import com.couchbase.client.core.error.context.ReducedKeyValueErrorContext
 import com.couchbase.client.core.io.CollectionIdentifier
-import com.couchbase.client.core.kv.CoreRangeScanItem
-import com.couchbase.client.core.kv.RangeScanOrchestrator
+import com.couchbase.client.core.kv.*
 import com.couchbase.client.core.msg.Request
 import com.couchbase.client.core.msg.Response
 import com.couchbase.client.core.msg.ResponseStatus.SUBDOC_FAILURE
@@ -39,6 +41,7 @@ import com.couchbase.client.core.msg.kv.KeyValueRequest
 import com.couchbase.client.core.msg.kv.MutationToken
 import com.couchbase.client.core.msg.kv.SubdocGetRequest
 import com.couchbase.client.core.msg.kv.SubdocMutateRequest
+import com.couchbase.client.core.retry.RetryStrategy
 import com.couchbase.client.kotlin.annotations.VolatileCouchbaseApi
 import com.couchbase.client.kotlin.codec.Content
 import com.couchbase.client.kotlin.codec.JsonSerializer
@@ -68,12 +71,14 @@ import com.couchbase.client.kotlin.kv.ScanSort
 import com.couchbase.client.kotlin.kv.ScanType
 import com.couchbase.client.kotlin.kv.StoreSemantics
 import com.couchbase.client.kotlin.util.StorageSize
+import com.couchbase.client.kotlin.util.StorageSizeUnit
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.future.await
 import kotlinx.coroutines.reactive.asFlow
 import kotlinx.coroutines.reactive.awaitFirstOrNull
 import reactor.core.publisher.Flux
+import java.util.*
 import kotlin.time.Duration
 import kotlin.time.toJavaDuration
 import kotlin.time.toKotlinDuration
@@ -248,43 +253,40 @@ public class Collection internal constructor(
         sort: ScanSort = ScanSort.NONE,
         idsOnly: Boolean = false,
         consistency: KvScanConsistency = KvScanConsistency.notBounded(),
-        batchItemLimit: Int = DEFAULT_SCAN_BATCH_ITEM_LIMIT,
-        batchSizeLimit: StorageSize = DEFAULT_SCAN_BATCH_SIZE_LIMIT,
+        batchItemLimit: Int = RangeScanOrchestrator.RANGE_SCAN_DEFAULT_BATCH_ITEM_LIMIT,
+        batchSizeLimit: StorageSize = StorageSize(
+            RangeScanOrchestrator.RANGE_SCAN_DEFAULT_BATCH_BYTE_LIMIT.toLong(),
+            StorageSizeUnit.BYTES
+        )
     ): Flux<CoreRangeScanItem> {
 
-        val batchByteLimit = batchSizeLimit.inBytes.toSaturatedInt()
-
-        val timeout = with(core.env) { common.actualKvScanTimeout() }
-
-        val consistentWith = mutableMapOf<Short, MutationToken>()
-        consistency.mutationState?.forEach { token -> consistentWith[token.partitionID()] = token }
+        val options = object : CoreScanOptions {
+            override fun commonOptions(): CoreCommonOptions = common.toCore()
+            override fun idsOnly(): Boolean = idsOnly
+            override fun sort(): CoreRangeScanSort = sort.toCore()
+            override fun consistentWith(): CoreMutationState = CoreMutationState(consistency.mutationState)
+            override fun batchItemLimit(): Int = batchItemLimit
+            override fun batchByteLimit(): Int = batchSizeLimit.inBytes.toSaturatedInt()
+        }
 
         return when (type) {
-            is ScanType.Sample -> rangeScanOrchestrator.samplingScan(
-                type.limit,
-                type.seed.toOptional(),
-                timeout,
-                batchItemLimit,
-                batchByteLimit,
-                idsOnly,
-                sort.toCore(),
-                common.parentSpan.toOptional(),
-                consistentWith,
-            )
+            is ScanType.Sample -> {
 
-            is ScanType.Range -> rangeScanOrchestrator.rangeScan(
-                type.from.term,
-                type.from.exclusive,
-                type.to.term,
-                type.to.exclusive,
-                timeout,
-                batchItemLimit,
-                batchByteLimit,
-                idsOnly,
-                sort.toCore(),
-                common.parentSpan.toOptional(),
-                consistentWith,
-            )
+                val samplingScan = object : CoreSamplingScan {
+                    override fun limit(): Long = type.limit
+                    override fun seed(): Optional<Long> = type.seed.toOptional();
+                }
+
+                return rangeScanOrchestrator.samplingScan(samplingScan, options)
+            }
+
+            is ScanType.Range -> {
+                val rangeScan = object : CoreRangeScan {
+                    override fun from(): CoreScanTerm = CoreScanTerm(type.from.term, type.from.exclusive)
+                    override fun to(): CoreScanTerm  = CoreScanTerm(type.to.term, type.to.exclusive)
+                }
+                rangeScanOrchestrator.rangeScan(rangeScan, options)
+            }
         }
     }
 

core-io/src/main/java/com/couchbase/client/core/api/shared/CoreMutationState.java
@@ -20,23 +20,29 @@ import com.couchbase.client.core.annotation.Stability;
 import com.couchbase.client.core.msg.kv.MutationToken;
 import com.couchbase.client.core.util.CbCollections;
 
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 @Stability.Internal
-public class CoreMutationState implements Iterable<MutationToken> {
+public class CoreMutationState {
   private final List<MutationToken> tokens;
 
   public CoreMutationState(Iterable<MutationToken> tokens) {
     this.tokens = CbCollections.listCopyOf(tokens);
   }
 
+  public Map<Short, MutationToken> toMap() {
+    Map<Short, MutationToken> map = new HashMap<>();
+    tokens.forEach(mt -> map.put(mt.partitionID(), mt));
+    return map;
+  }
+
   public List<MutationToken> tokens() {
     return tokens;
   }
 
-  @Override
-  public Iterator<MutationToken> iterator() {
-    return tokens.iterator();
-  }
 }

core-io/src/main/java/com/couchbase/client/core/classic/kv/ClassicCoreKvOps.java
@@ -786,27 +786,12 @@ public final class ClassicCoreKvOps implements CoreKvOps {
     notNull(scanType, "ScanType");
     notNull(options, "Options");
 
-    Duration timeout = options.commonOptions().timeout().orElse(core.context().environment().timeoutConfig().kvScanTimeout());
-
-    Map<Short, MutationToken> consistencyTokens = options.consistentWith().map(ms -> {
-      Map<Short, MutationToken> tokens = new HashMap<>();
-      for (MutationToken mt : ms) {
-        tokens.put(mt.partitionID(), mt);
-      }
-      return tokens;
-    }).orElse(Collections.emptyMap());
-
     Flux<CoreRangeScanItem> coreScanStream;
 
     if (scanType instanceof CoreRangeScan) {
-      CoreRangeScan rs = (CoreRangeScan) scanType;
-      coreScanStream = rangeScanOrchestrator.rangeScan(rs.from().id(), rs.from().exclusive(), rs.to().id(),
-          rs.to().exclusive(), timeout, options.batchItemLimit(), options.batchByteLimit(), options.idsOnly(), options.sort(),
-          options.commonOptions().parentSpan(), consistencyTokens);
+      coreScanStream = rangeScanOrchestrator.rangeScan((CoreRangeScan) scanType, options);
     } else if (scanType instanceof CoreSamplingScan) {
-      CoreSamplingScan ss = (CoreSamplingScan) scanType;
-      coreScanStream = rangeScanOrchestrator.samplingScan(ss.limit(), ss.seed(), timeout, options.batchItemLimit(),
-          options.batchByteLimit(), options.idsOnly(), options.sort(), options.commonOptions().parentSpan(), consistencyTokens);
+      coreScanStream = rangeScanOrchestrator.samplingScan((CoreSamplingScan) scanType,options);
     } else {
       return Flux.error(InvalidArgumentException.fromMessage("Unsupported ScanType: " + scanType));
     }

core-io/src/main/java/com/couchbase/client/core/endpoint/http/CoreCommonOptions.java
@@ -18,7 +18,6 @@ package com.couchbase.client.core.endpoint.http;
 
 import com.couchbase.client.core.annotation.Stability;
 import com.couchbase.client.core.cnc.RequestSpan;
-import com.couchbase.client.core.msg.Request;
 import com.couchbase.client.core.retry.RetryStrategy;
 import reactor.util.annotation.Nullable;
 
@@ -103,4 +102,5 @@ public interface CoreCommonOptions {
   default CoreCommonOptions withParentSpan(RequestSpan span) {
     return CoreCommonOptions.of(timeout().orElse(null), retryStrategy().orElse(null), span);
   }
+
 }

core-io/src/main/java/com/couchbase/client/core/kv/CoreScanOptions.java
@@ -17,11 +17,19 @@
 package com.couchbase.client.core.kv;
 
 import java.time.Duration;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 
+import com.couchbase.client.core.CoreContext;
 import com.couchbase.client.core.annotation.Stability;
 import com.couchbase.client.core.api.shared.CoreMutationState;
+import com.couchbase.client.core.cnc.RequestSpan;
 import com.couchbase.client.core.endpoint.http.CoreCommonOptions;
+import com.couchbase.client.core.msg.kv.MutationToken;
+import com.couchbase.client.core.retry.RetryStrategy;
+import reactor.util.annotation.Nullable;
 
 /**
  * Allows to customize the various range and sampling scan options.
@@ -29,16 +37,21 @@ import com.couchbase.client.core.endpoint.http.CoreCommonOptions;
 @Stability.Internal
 public interface CoreScanOptions {
 
+  CoreCommonOptions commonOptions();
+
   boolean idsOnly();
 
-  Optional<CoreMutationState> consistentWith();
-
+  @Nullable
   CoreRangeScanSort sort();
+
+  @Nullable
+  CoreMutationState consistentWith();
 
   int batchItemLimit();
 
   int batchByteLimit();
 
-  CoreCommonOptions commonOptions();
-
+  default Map<Short, MutationToken> consistencyMap(){
+    return consistentWith() == null ? new HashMap<>() : consistentWith().toMap();
+  }
 }

core-io/src/main/java/com/couchbase/client/core/kv/CoreScanTerm.java
@@ -15,21 +15,31 @@
  */
 package com.couchbase.client.core.kv;
 
-import static com.couchbase.client.core.util.Validators.notNullOrEmpty;
-
-import java.nio.charset.StandardCharsets;
-
 import com.couchbase.client.core.annotation.Stability;
+
+import static com.couchbase.client.core.util.Validators.notNullOrEmpty;
 
 /**
  * A single {@link CoreScanTerm} identifying either the point to scan from or to scan to.
  */
 @Stability.Internal
-public interface CoreScanTerm {
+public class CoreScanTerm {
 
-  byte[] id();
+  private final byte[] id;
+  private final boolean exclusive;
 
-  boolean exclusive();
+  public CoreScanTerm(byte[] id, boolean exclusive){
+    this.id = notNullOrEmpty(id, "ScanTerm ID").clone();
+    this.exclusive = exclusive;
+  }
+
+  public byte[] id(){
+    return id.clone();
+  }
+
+  public boolean exclusive(){
+    return exclusive;
+  }
 
 }
 

core-io/src/main/java/com/couchbase/client/core/kv/RangeScanOrchestrator.java
@@ -17,11 +17,10 @@
 package com.couchbase.client.core.kv;
 
 import com.couchbase.client.core.Core;
-import com.couchbase.client.core.CoreContext;
 import com.couchbase.client.core.Reactor;
 import com.couchbase.client.core.annotation.Stability;
+import com.couchbase.client.core.api.shared.CoreMutationState;
 import com.couchbase.client.core.cnc.RequestSpan;
-import com.couchbase.client.core.cnc.TracingIdentifiers;
 import com.couchbase.client.core.config.BucketCapabilities;
 import com.couchbase.client.core.config.BucketConfig;
 import com.couchbase.client.core.config.CouchbaseBucketConfig;
@@ -43,6 +42,7 @@ import com.couchbase.client.core.msg.ResponseStatus;
 import com.couchbase.client.core.msg.kv.MutationToken;
 import com.couchbase.client.core.msg.kv.RangeScanContinueRequest;
 import com.couchbase.client.core.msg.kv.RangeScanCreateRequest;
+import com.couchbase.client.core.retry.RetryStrategy;
 import reactor.core.Exceptions;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -116,67 +116,40 @@ public class RangeScanOrchestrator {
   /**
    * Performs a range scan between a start and an end term (reactive).
    *
-   * @param startTerm the start term used for the range scan.
-   * @param startExclusive if the start term is exclusive or inclusive.
-   * @param endTerm the end term used for the range scan.
-   * @param endExclusive if the end term is exclusive or inclusive.
-   * @param timeout the timeout for the full operation.
-   * @param continueItemLimit the number of items to load (max) per continue operation.
-   * @param continueByteLimit the number of bytes to load (max) per continue operation.
-   * @param keysOnly if only keys should be loaded.
-   * @param sort the sorting to apply (could be none).
-   * @param parent the optional span parent to use.
+   * @param rangeScan
+   * @param options
    * @return a {@link Flux} of returned items, or a failed flux during errors.
    */
-  public Flux<CoreRangeScanItem> rangeScan(byte[] startTerm, boolean startExclusive, byte[] endTerm,
-                                           boolean endExclusive, Duration timeout, int continueItemLimit,
-                                           int continueByteLimit, boolean keysOnly, CoreRangeScanSort sort,
-                                           Optional<RequestSpan> parent, Map<Short, MutationToken> consistencyTokens) {
+
+    public Flux<CoreRangeScanItem> rangeScan(CoreRangeScan rangeScan, CoreScanOptions options) {
     return Flux.defer(() -> {
+
       if (currentBucketConfig == null) {
         // We might not have a config yet if bootstrap is still in progress, wait 100ms
         // and then try again. In a steady state this should not happen.
         return Mono
           .delay(Duration.ofMillis(100), core.context().environment().scheduler())
-          .flatMapMany(ign -> rangeScan(startTerm, startExclusive, endTerm, endExclusive, timeout,
-            continueItemLimit, continueByteLimit, keysOnly, sort, parent, consistencyTokens));
+          .flatMapMany(ign -> rangeScan(rangeScan, options));
       } else if (!(currentBucketConfig instanceof CouchbaseBucketConfig)) {
         return Flux.error(new IllegalStateException("Only Couchbase buckets are supported with KV Range Scan"));
       }
-
+      Map<Short, MutationToken> consistencyMap=options.consistencyMap();
       return streamForPartitions((partition, start) -> {
-        CoreContext ctx = core.context();
-        RequestSpan span = ctx
-          .environment()
-          .requestTracer()
-          .requestSpan(TracingIdentifiers.SPAN_REQUEST_KV_RANGE_SCAN_CREATE, parent.orElse(null));
-
-        Optional<MutationToken> mutationToken = Optional.ofNullable(consistencyTokens.get(partition));
-
-        byte[] actualStartTerm = start == null ? startTerm : start;
-        return RangeScanCreateRequest.forRangeScan(actualStartTerm, startExclusive, endTerm, endExclusive, keysOnly, timeout,
-          core.context(), core.context().environment().retryStrategy(), collectionIdentifier, span, partition,
-          mutationToken);
-      }, sort, timeout, continueItemLimit, continueByteLimit, parent, keysOnly);
+        byte[] actualStartTerm = start == null ? rangeScan.from().id() : start;
+        return RangeScanCreateRequest.forRangeScan(actualStartTerm, rangeScan, options, partition, core.context(),
+            collectionIdentifier, consistencyMap);
+      }, options);
     });
   }
 
   /**
    * Performs a sampling scan (reactive).
    *
-   * @param limit the number of items to load for the sampling scan.
-   * @param seed the seed number to be used.
-   * @param timeout the timeout for the full operation.
-   * @param continueItemLimit the number of items to load (max) per continue operation.
-   * @param continueByteLimit the number of bytes to load (max) per continue operation.
-   * @param keysOnly if only keys should be loaded.
-   * @param sort the sorting to apply (could be none).
-   * @param parent the optional span parent to use.
+   * @param samplingScan
+   * @param options
    * @return a {@link Flux} of returned items, or a failed flux during errors.
    */
-  public Flux<CoreRangeScanItem> samplingScan(long limit, Optional<Long> seed, Duration timeout, int continueItemLimit,
-                                              int continueByteLimit, boolean keysOnly, CoreRangeScanSort sort,
-                                              Optional<RequestSpan> parent, Map<Short, MutationToken> consistencyTokens) {
+  public Flux<CoreRangeScanItem> samplingScan(CoreSamplingScan samplingScan, CoreScanOptions options) {
     return Flux
       .defer(() -> {
         if (currentBucketConfig == null) {
@@ -184,35 +157,19 @@ public class RangeScanOrchestrator {
           // and then try again. In a steady state this should not happen.
           return Mono
             .delay(Duration.ofMillis(100), core.context().environment().scheduler())
-            .flatMapMany(ign -> samplingScan(limit, seed, timeout, continueItemLimit, continueByteLimit,
-              keysOnly, sort, parent, consistencyTokens));
+            .flatMapMany(ign -> samplingScan(samplingScan, options));
         } else if (!(currentBucketConfig instanceof CouchbaseBucketConfig)) {
           return Flux.error(new IllegalStateException("Only Couchbase buckets are supported with KV Range Scan"));
         }
-
-        return streamForPartitions((partition, ignored) -> {
-          CoreContext ctx = core.context();
-          RequestSpan span = ctx
-            .environment()
-            .requestTracer()
-            .requestSpan(TracingIdentifiers.SPAN_REQUEST_KV_RANGE_SCAN_CREATE, parent.orElse(null));
-
-          Optional<MutationToken> mutationToken = Optional.ofNullable(consistencyTokens.get(partition));
-
-          return RangeScanCreateRequest.forSamplingScan(limit, seed, keysOnly, timeout, ctx,
-            ctx.environment().retryStrategy(), collectionIdentifier, span, partition, mutationToken);
-        }, sort, timeout, continueItemLimit, continueByteLimit, parent, keysOnly);
-
-      })
-      .take(limit);
+        Map<Short, MutationToken> consistencyMap=options.consistencyMap();
+        return streamForPartitions((partition, ignored) -> RangeScanCreateRequest.forSamplingScan(samplingScan,
+              options, partition, core.context(), collectionIdentifier, consistencyMap), options);
+        }).take(samplingScan.limit());
   }
 
   @SuppressWarnings("unchecked")
   private Flux<CoreRangeScanItem> streamForPartitions(final BiFunction<Short, byte[], RangeScanCreateRequest> createSupplier,
-                                                      final CoreRangeScanSort sort,
-                                                      final Duration timeout, final int itemLimit,
-                                                      final int byteLimit, Optional<RequestSpan> parent,
-                                                      final boolean keysOnly) {
+                                                      final CoreScanOptions options) {
 
     if (!capabilityEnabled) {
       return Flux.error(FeatureNotAvailableException.rangeScan());
@@ -224,12 +181,12 @@ public class RangeScanOrchestrator {
 
     List<Flux<CoreRangeScanItem>> partitionStreams = new ArrayList<>(numPartitions);
     for (short i = 0; i < numPartitions; i++) {
-      partitionStreams.add(streamForPartition(i, createSupplier, timeout, itemLimit, byteLimit, parent, keysOnly));
+      partitionStreams.add(streamForPartition(i, createSupplier, options));
     }
 
     Flux<CoreRangeScanItem> stream;
 
-    if (CoreRangeScanSort.ASCENDING == sort) {
+    if (CoreRangeScanSort.ASCENDING == options.sort()) {
       stream = Flux.mergeComparing(
         Comparator.comparing(CoreRangeScanItem::key),
         partitionStreams.toArray(new Flux[0])
@@ -240,15 +197,14 @@ public class RangeScanOrchestrator {
 
     return stream
       .doOnNext(item -> itemsStreamed.incrementAndGet())
-      .timeout(timeout, Mono.defer(() -> Mono.error(
+      .timeout(options.commonOptions().timeout().orElse(core.context().environment().timeoutConfig().kvScanTimeout()), Mono.defer(() -> Mono.error(
         new UnambiguousTimeoutException("RangeScan timed out", new CancellationErrorContext(new RangeScanContext(itemsStreamed.get())))
       )));
   }
 
   private Flux<CoreRangeScanItem> streamForPartition(final short partition,
                                                      final BiFunction<Short, byte[], RangeScanCreateRequest> createSupplier,
-                                                     final Duration timeout, final int itemLimit, final int byteLimit,
-                                                     Optional<RequestSpan> parent, final boolean keysOnly) {
+                                                     final CoreScanOptions options) {
     final AtomicReference<byte[]> lastStreamed = new AtomicReference<>();
 
     return Flux
@@ -259,7 +215,7 @@ public class RangeScanOrchestrator {
           .wrap(request, request.response(), true)
           .flatMapMany(res -> {
             if (res.status().success()) {
-              return continueScan(timeout, partition, res.rangeScanId(), itemLimit, byteLimit, parent, keysOnly);
+              return continueScan( partition, res.rangeScanId(), options);
             }
 
             final KeyValueErrorContext errorContext = KeyValueErrorContext.completedRequest(request, res);
@@ -286,25 +242,15 @@ public class RangeScanOrchestrator {
       })));
   }
 
-  private Flux<CoreRangeScanItem> continueScan(final Duration timeout, final short partition, final CoreRangeScanId id,
-                                               final int itemLimit, final int byteLimit, Optional<RequestSpan> parent,
-                                               final boolean keysOnly) {
+  private Flux<CoreRangeScanItem> continueScan(final short partition, final CoreRangeScanId id,
+                                               final CoreScanOptions options) {
     final AtomicBoolean complete = new AtomicBoolean(false);
 
     return Flux
       .defer(() -> {
-        CoreContext ctx = core.context();
-
-        RequestSpan span = ctx
-          .environment()
-          .requestTracer()
-          .requestSpan(TracingIdentifiers.SPAN_REQUEST_KV_RANGE_SCAN_CONTINUE, parent.orElse(null));
-
-        RangeScanContinueRequest request = new RangeScanContinueRequest(id, itemLimit, byteLimit, timeout, ctx,
-          ctx.environment().retryStrategy(), null, collectionIdentifier, span,
-          Sinks.many().unicast().onBackpressureBuffer(), partition, keysOnly
-        );
-        core.send(request);
+          RangeScanContinueRequest request = new RangeScanContinueRequest(id,
+              Sinks.many().unicast().onBackpressureBuffer(), null, options, partition, core.context(), collectionIdentifier);
+          core.send(request);
         return Reactor
           .wrap(request, request.response(), true)
           .flatMapMany(res -> {

core-io/src/main/java/com/couchbase/client/core/msg/kv/RangeScanContinueRequest.java
@@ -17,7 +17,7 @@
 package com.couchbase.client.core.msg.kv;
 
 import com.couchbase.client.core.CoreContext;
-import com.couchbase.client.core.cnc.RequestSpan;
+import com.couchbase.client.core.cnc.TracingIdentifiers;
 import com.couchbase.client.core.deps.io.netty.buffer.ByteBuf;
 import com.couchbase.client.core.deps.io.netty.buffer.ByteBufAllocator;
 import com.couchbase.client.core.io.CollectionIdentifier;
@@ -25,11 +25,10 @@ import com.couchbase.client.core.io.netty.kv.KeyValueChannelContext;
 import com.couchbase.client.core.io.netty.kv.MemcacheProtocol;
 import com.couchbase.client.core.kv.CoreRangeScanId;
 import com.couchbase.client.core.kv.CoreRangeScanItem;
+import com.couchbase.client.core.kv.CoreScanOptions;
 import com.couchbase.client.core.msg.ResponseStatus;
-import com.couchbase.client.core.retry.RetryStrategy;
 import reactor.core.publisher.Sinks;
 
-import java.time.Duration;
 
 import static com.couchbase.client.core.io.netty.kv.MemcacheProtocol.Opcode.RANGE_SCAN_CONTINUE;
 import static com.couchbase.client.core.io.netty.kv.MemcacheProtocol.decodeStatus;
@@ -48,17 +47,30 @@ public class RangeScanContinueRequest extends PredeterminedPartitionRequest<Rang
 
   private final boolean keysOnly;
 
-  public RangeScanContinueRequest(CoreRangeScanId id, int itemLimit, int byteLimit, Duration timeout, CoreContext ctx,
-                                  RetryStrategy retryStrategy, String key, CollectionIdentifier collectionIdentifier,
-                                  RequestSpan span, Sinks.Many<CoreRangeScanItem> sink, short partition, boolean keysOnly) {
-    super(partition, timeout, ctx, retryStrategy, key, collectionIdentifier, span);
+  public RangeScanContinueRequest(CoreRangeScanId id,
+                                  Sinks.Many<CoreRangeScanItem> sink,
+                                  String key,
+                                  CoreScanOptions options,
+                                  short partition,
+                                  CoreContext ctx,
+                                  CollectionIdentifier collectionIdentifier ) {
+
+    super(
+      partition,
+      options.commonOptions().timeout().orElse(ctx.environment().timeoutConfig().kvScanTimeout()),
+      ctx,
+      options.commonOptions().retryStrategy().orElse(ctx.environment().retryStrategy()),
+      key,
+      collectionIdentifier,
+      ctx.environment().requestTracer().requestSpan(TracingIdentifiers.SPAN_REQUEST_KV_RANGE_SCAN_CREATE, options.commonOptions().parentSpan().orElse(null)));
     this.id = id;
-    this.itemLimit = itemLimit;
-    this.byteLimit = byteLimit;
-    this.timeLimit = Math.toIntExact(timeout.toMillis());
+    this.itemLimit = options.batchItemLimit();
+    this.byteLimit = options.batchByteLimit();
+    this.timeLimit = Math.toIntExact(timeout().toMillis());
     this.sink = sink;
-    this.keysOnly = keysOnly;
+    this.keysOnly = options.idsOnly();
   }
+
 
   @Override
   public ByteBuf encode(final ByteBufAllocator alloc, final int opaque, final KeyValueChannelContext ctx) {

commit e9b87554dc52f4798f82af82131925fc965fcd4e

Kotlin: Range scan throws NPE when scan consistency is unbounded
Fixes a regression in ee85847.

Change-Id: I730a71a2896b101968ce5f557cc5993d1cf2e8b2
Reviewed-on: https://review.couchbase.org/c/couchbase-jvm-clients/+/188908
Tested-by: Build Bot <build@couchbase.com>
Reviewed-by: David Nault <david.nault@couchbase.com>

kotlin-client/src/main/kotlin/com/couchbase/client/kotlin/Collection.kt
@@ -270,7 +270,7 @@ public class Collection internal constructor(
         val options = object : CoreScanOptions {
             override fun commonOptions(): CoreCommonOptions = common.toCore()
             override fun idsOnly(): Boolean = idsOnly
-            override fun consistentWith(): CoreMutationState = CoreMutationState(consistency.mutationState)
+            override fun consistentWith(): CoreMutationState? = consistency.mutationState?.let { CoreMutationState(consistency.mutationState) }
             override fun batchItemLimit(): Int = batchItemLimit
             override fun batchByteLimit(): Int = batchSizeLimit.inBytes.toSaturatedInt()
         }