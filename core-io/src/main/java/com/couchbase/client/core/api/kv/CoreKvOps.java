couchbase/couchbase-jvm-clients

commit 79df52e4002f72710baffa9b18ea2460f8768951

JVMCBC-1216 Move lookupIn into core
Modifications
-------------
Add CoreKvOps.subdocGet* and a Classic implementation.
Modify Java and Kotlin to use it.

Remove LookupInResult.equals() and hashcode().
They were broken and only provided identity equality
because SubDocumentField does not implement equals()
or hashCode().

Make Kotlin LookupInResult constructor internal.
It was already implicitly internal API because
SubDocumentField is internal API, so hopefully this
won't disappoint anyone.

Change-Id: I00ad4f236c1bbf2910bf36704ba84a50729252ae
Reviewed-on: https://review.couchbase.org/c/couchbase-jvm-clients/+/185819
Tested-by: Build Bot <build@couchbase.com>
Reviewed-by: Graham Pople <graham.pople@couchbase.com>

core-io/src/main/java/com/couchbase/client/core/api/kv/CoreKvOps.java
@@ -299,6 +299,31 @@ public interface CoreKvOps {
     return Mono.defer(() -> unlockAsync(common, key, cas).toMono());
   }
 
+  CoreAsyncResponse<CoreSubdocGetResult> subdocGetAsync(
+      CoreCommonOptions common,
+      String key,
+      List<CoreSubdocGetCommand> commands,
+      boolean accessDeleted
+  );
+
+  default CoreSubdocGetResult subdocGetBlocking(
+      CoreCommonOptions common,
+      String key,
+      List<CoreSubdocGetCommand> commands,
+      boolean accessDeleted
+  ) {
+    return subdocGetAsync(common, key, commands, accessDeleted).toBlocking();
+  }
+
+  default Mono<CoreSubdocGetResult> subdocGetReactive(
+      CoreCommonOptions common,
+      String key,
+      List<CoreSubdocGetCommand> commands,
+      boolean accessDeleted
+  ) {
+    return Mono.defer(() -> subdocGetReactive(common, key, commands, accessDeleted));
+  }
+
   Flux<CoreGetResult> getAllReplicasReactive(
       CoreCommonOptions common,
       String key

java-client/src/integrationTest/java/com/couchbase/client/java/SubdocIntegrationTest.java
@@ -22,10 +22,10 @@ import com.couchbase.client.core.error.DocumentNotFoundException;
 import com.couchbase.client.core.error.InvalidArgumentException;
 import com.couchbase.client.core.error.subdoc.DocumentNotJsonException;
 import com.couchbase.client.core.error.subdoc.PathInvalidException;
+import com.couchbase.client.core.error.subdoc.PathMismatchException;
 import com.couchbase.client.core.error.subdoc.PathNotFoundException;
 import com.couchbase.client.core.error.subdoc.XattrUnknownVirtualAttributeException;
 import com.couchbase.client.core.msg.kv.DurabilityLevel;
-import com.couchbase.client.core.util.CbThrowables;
 import com.couchbase.client.java.codec.RawStringTranscoder;
 import com.couchbase.client.java.codec.TypeRef;
 import com.couchbase.client.java.json.JsonArray;
@@ -51,12 +51,10 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 
 import static com.couchbase.client.core.util.CbCollections.listOf;
 import static com.couchbase.client.core.util.CbCollections.mapOf;
-import static com.couchbase.client.core.util.CbThrowables.findCause;
 import static com.couchbase.client.core.util.CbThrowables.hasCause;
 import static com.couchbase.client.java.kv.LookupInSpec.get;
 import static java.nio.charset.StandardCharsets.UTF_8;
@@ -306,7 +304,9 @@ class SubdocIntegrationTest extends JavaIntegrationTest {
     LookupInResult result = collection.lookupIn(id, Collections.singletonList(LookupInSpec.exists("not_exist")));
 
     assertFalse(result.exists(0));
-    assertThrows(PathNotFoundException.class, () -> assertTrue(result.contentAs(0, Boolean.class)));
+
+    // Bug: https://issues.couchbase.com/browse/JCBC-2056
+    assertThrows(PathNotFoundException.class, () -> result.contentAs(0, Boolean.class));
   }
 
   @Test
@@ -328,26 +328,28 @@ class SubdocIntegrationTest extends JavaIntegrationTest {
 
     collection.upsert(id, JsonObject.create().put("foo", "bar"));
 
-
     LookupInResult result = collection.lookupIn(
       id,
-      Arrays.asList(LookupInSpec.exists("not_exist"), LookupInSpec.get("foo"), LookupInSpec.exists("foo"))
+      listOf(
+        LookupInSpec.exists("not_exist"),
+        LookupInSpec.get("foo"),
+        LookupInSpec.exists("foo")
+      )
     );
 
     assertFalse(result.exists(0));
-    assertThrows(
-      PathNotFoundException.class,
-      () -> assertTrue(result.contentAs(0, Boolean.class))
-    );
+
+    // Bug: https://issues.couchbase.com/browse/JCBC-2056
+    assertThrows(PathNotFoundException.class, () -> result.contentAs(0, Boolean.class));
+    assertThrows(PathNotFoundException.class, () -> result.contentAsBytes(0));
 
     assertTrue(result.exists(1));
     assertEquals("bar", result.contentAs(1, String.class));
 
     assertTrue(result.exists(2));
     assertTrue(result.contentAs(2, Boolean.class));
-    assertThrows(
-            IllegalArgumentException.class,
-            () -> result.contentAs(2, String.class));
+    assertEquals("true", result.contentAs(2, String.class));
+    assertArrayEquals("true".getBytes(UTF_8), result.contentAsBytes(2));
   }
 
   @Test
@@ -543,5 +545,38 @@ class SubdocIntegrationTest extends JavaIntegrationTest {
 
     assertThrows(XattrUnknownVirtualAttributeException.class, () ->
             collection.lookupIn(id, Arrays.asList(LookupInSpec.get("$vbucket").xattr())));
+  }
+
+  @Test
+  void existsAcceptsInvalidIndex() {
+    String id = UUID.randomUUID().toString();
+
+    collection.upsert(id, mapOf());
+
+    LookupInResult result = collection.lookupIn(id, listOf(
+      LookupInSpec.exists("doesNotExist")
+    ));
+
+    // This behavior is not according to spec, but it's what the
+    // Java SDK currently does.
+    assertFalse(result.exists(7));
+  }
+
+  @Test
+  void existsDoesNotPropagateExceptions() {
+    String id = UUID.randomUUID().toString();
+
+    collection.upsert(id, mapOf("foo", mapOf("bar", "zor")));
+
+    LookupInResult result = collection.lookupIn(id, listOf(
+      LookupInSpec.get("foo.bar[0]")
+    ));
+
+    assertThrows(PathMismatchException.class, () -> result.contentAsBytes(0));
+
+    // Spec says `exists` should propagate all exceptions except "PATH_NOT_FOUND",
+    // just like `get` or `count`.
+    // Java SDK diverges from spec; it treats all errors as "does not exit".
+    assertFalse(result.exists(0));
   }
 }

core-io/src/main/java/com/couchbase/client/core/api/kv/CoreKvParamValidators.java
@@ -83,6 +83,12 @@ public class CoreKvParamValidators {
     }
   }
 
+  public static void validateSubdocGetParams(CoreCommonOptions common, String key, List<CoreSubdocGetCommand> commands) {
+    validateCommonOptions(common, key);
+    notNullOrEmpty(commands, "At least one lookup command is required.");
+    // Server validates command limit.
+  }
+
   public static void validateGetAllReplicasParams(CoreCommonOptions common, String key) {
     validateCommonOptions(common, key);
   }

core-io/src/main/java/com/couchbase/client/core/api/kv/CoreSubdocGetCommand.java
@@ -1,0 +1,58 @@
+/*
+ * Copyright 2023 Couchbase, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.couchbase.client.core.api.kv;
+
+import com.couchbase.client.core.annotation.Stability;
+import com.couchbase.client.core.msg.kv.SubdocCommandType;
+
+import static com.couchbase.client.core.logging.RedactableArgument.redactUser;
+import static java.util.Objects.requireNonNull;
+
+@Stability.Internal
+public class CoreSubdocGetCommand {
+  private final SubdocCommandType type;
+  private final String path;
+  private final boolean xattr;
+
+  public CoreSubdocGetCommand(SubdocCommandType type, String path, boolean xattr) {
+    this.type = requireNonNull(type);
+    this.path = requireNonNull(path);
+    this.xattr = xattr;
+  }
+
+  public SubdocCommandType type() {
+    return type;
+  }
+
+  public String path() {
+    return path;
+  }
+
+  public boolean xattr() {
+    return xattr;
+  }
+
+  @Override
+  public String toString() {
+    return "CoreSubdocGetCommand{" +
+        "type=" + type +
+        ", path='" + redactUser(path) + '\'' +
+        ", xattr=" + xattr +
+        '}';
+  }
+}
+

core-io/src/main/java/com/couchbase/client/core/api/kv/CoreSubdocGetResult.java
@@ -1,0 +1,96 @@
+/*
+ * Copyright 2023 Couchbase, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.couchbase.client.core.api.kv;
+
+import com.couchbase.client.core.CoreKeyspace;
+import com.couchbase.client.core.annotation.Stability;
+import com.couchbase.client.core.error.CouchbaseException;
+import com.couchbase.client.core.error.InvalidArgumentException;
+import com.couchbase.client.core.error.subdoc.PathNotFoundException;
+import com.couchbase.client.core.msg.kv.SubDocumentField;
+import reactor.util.annotation.Nullable;
+
+import java.util.List;
+
+import static com.couchbase.client.core.logging.RedactableArgument.redactUser;
+import static java.util.Objects.requireNonNull;
+
+@Stability.Internal
+public class CoreSubdocGetResult extends CoreKvResult {
+  private final long cas;
+  private final boolean tombstone;
+  private final List<SubDocumentField> fields;
+
+  public CoreSubdocGetResult(
+      CoreKeyspace keyspace,
+      String key,
+      @Nullable CoreKvResponseMetadata meta,
+      List<SubDocumentField> fields,
+      long cas,
+      boolean tombstone
+  ) {
+    super(keyspace, key, meta);
+    this.fields = requireNonNull(fields);
+    this.cas = cas;
+    this.tombstone = tombstone;
+  }
+
+  public List<SubDocumentField> fields() {
+    return fields;
+  }
+
+  public SubDocumentField field(int index) {
+    try {
+      return fields.get(index);
+    } catch (IndexOutOfBoundsException e) {
+      throw InvalidArgumentException.fromMessage("Index " + index + " is out of bounds; must be >= 0 and < " + fields.size(), e);
+    }
+  }
+
+  public boolean exists(int index) {
+    SubDocumentField field = field(index);
+    CouchbaseException error = field.error().orElse(null);
+
+    if (error == null) {
+      return true;
+    }
+
+    if (error instanceof PathNotFoundException) {
+      return false;
+    }
+
+    // Propagate all other errors (path mismatch, document not json, etc.)
+    throw error;
+  }
+
+  public long cas() {
+    return cas;
+  }
+
+  public boolean tombstone() {
+    return tombstone;
+  }
+
+  @Override
+  public String toString() {
+    return "CoreSubdocGetResult{" +
+        "cas=" + cas +
+        ", tombstone=" + tombstone +
+        ", fields=" + redactUser(fields) +
+        '}';
+  }
+}

core-io/src/main/java/com/couchbase/client/core/classic/kv/ClassicCoreKvOps.java
@@ -31,6 +31,8 @@ import com.couchbase.client.core.api.kv.CoreKvResponseMetadata;
 import com.couchbase.client.core.api.kv.CoreLookupInMacro;
 import com.couchbase.client.core.api.kv.CoreMutationResult;
 import com.couchbase.client.core.api.kv.CoreStoreSemantics;
+import com.couchbase.client.core.api.kv.CoreSubdocGetCommand;
+import com.couchbase.client.core.api.kv.CoreSubdocGetResult;
 import com.couchbase.client.core.api.kv.CoreSubdocMutateCommand;
 import com.couchbase.client.core.api.kv.CoreSubdocMutateResult;
 import com.couchbase.client.core.classic.ClassicHelper;
@@ -100,6 +102,7 @@ import static com.couchbase.client.core.api.kv.CoreKvParamValidators.validateGet
 import static com.couchbase.client.core.api.kv.CoreKvParamValidators.validateInsertParams;
 import static com.couchbase.client.core.api.kv.CoreKvParamValidators.validateRemoveParams;
 import static com.couchbase.client.core.api.kv.CoreKvParamValidators.validateReplaceParams;
+import static com.couchbase.client.core.api.kv.CoreKvParamValidators.validateSubdocGetParams;
 import static com.couchbase.client.core.api.kv.CoreKvParamValidators.validateSubdocMutateParams;
 import static com.couchbase.client.core.api.kv.CoreKvParamValidators.validateTouchParams;
 import static com.couchbase.client.core.api.kv.CoreKvParamValidators.validateUnlockParams;
@@ -650,6 +653,54 @@ public final class ClassicCoreKvOps implements CoreKvOps {
           throw keyValueStatusToException(req, res);
         },
         it -> null
+    );
+  }
+
+  @Override
+  public CoreAsyncResponse<CoreSubdocGetResult> subdocGetAsync(
+      CoreCommonOptions common,
+      String key,
+      List<CoreSubdocGetCommand> commands,
+      boolean accessDeleted
+  ) {
+    validateSubdocGetParams(common, key, commands);
+
+    Duration timeout = timeout(common);
+    RetryStrategy retryStrategy = retryStrategy(common);
+    RequestSpan span = span(common, TracingIdentifiers.SPAN_REQUEST_KV_LOOKUP_IN);
+
+    byte flags = accessDeleted ? SubdocMutateRequest.SUBDOC_DOC_FLAG_ACCESS_DELETED : 0;
+
+    SubdocGetRequest request = SubdocGetRequest.create(
+        timeout,
+        ctx,
+        collectionIdentifier,
+        retryStrategy,
+        key,
+        flags,
+        commands,
+        span
+    );
+    request.context()
+        .clientContext(common.clientContext());
+
+    return newAsyncResponse(
+        request,
+        (req, res) -> {
+          if (res.status() == SUBDOC_FAILURE) {
+            // Ignore. The failure of any one lookup command does not cause the whole request to fail.
+            return;
+          }
+          commonKvResponseCheck(req, res);
+        },
+        it -> new CoreSubdocGetResult(
+            keyspace,
+            key,
+            CoreKvResponseMetadata.from(it.flexibleExtras()),
+            Arrays.asList(it.values()),
+            it.cas(),
+            it.isDeleted()
+        )
     );
   }
 

core-io/src/main/java/com/couchbase/client/core/msg/kv/SubDocumentField.java
@@ -18,11 +18,13 @@ package com.couchbase.client.core.msg.kv;
 
 import com.couchbase.client.core.annotation.Stability;
 import com.couchbase.client.core.error.CouchbaseException;
+import reactor.util.annotation.Nullable;
 
 import java.io.Serializable;
 import java.util.Optional;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
+import static java.util.Objects.requireNonNull;
 
 @Stability.Internal
 public class SubDocumentField implements Serializable {
@@ -30,18 +32,18 @@ public class SubDocumentField implements Serializable {
   private static final long serialVersionUID = 227930811730226484L;
 
   private final SubDocumentOpResponseStatus status;
-  private final CouchbaseException error;
+  @Nullable private final CouchbaseException error;
   private final byte[] value;
   private final String path;
   private final SubdocCommandType type;
 
   public SubDocumentField(SubDocumentOpResponseStatus status, Optional<CouchbaseException> error, byte[] value,
                           String path, SubdocCommandType type) {
-    this.status = status;
+    this.status = requireNonNull(status);
     this.error = error.orElse(null);
-    this.value = value;
-    this.path = path;
-    this.type = type;
+    this.value = requireNonNull(value);
+    this.path = requireNonNull(path);
+    this.type = requireNonNull(type);
   }
 
   public SubDocumentOpResponseStatus status() {
@@ -50,6 +52,12 @@ public class SubDocumentField implements Serializable {
 
   public Optional<CouchbaseException> error() {
     return Optional.ofNullable(error);
+  }
+
+  public void throwErrorIfPresent() {
+    if (error != null) {
+      throw error;
+    }
   }
 
   public byte[] value() {

core-io/src/main/java/com/couchbase/client/core/msg/kv/SubdocGetRequest.java
@@ -17,42 +17,64 @@
 package com.couchbase.client.core.msg.kv;
 
 import com.couchbase.client.core.CoreContext;
+import com.couchbase.client.core.api.kv.CoreSubdocGetCommand;
 import com.couchbase.client.core.cnc.RequestSpan;
 import com.couchbase.client.core.cnc.TracingIdentifiers;
+import com.couchbase.client.core.deps.io.netty.buffer.ByteBuf;
+import com.couchbase.client.core.deps.io.netty.buffer.ByteBufAllocator;
+import com.couchbase.client.core.deps.io.netty.buffer.CompositeByteBuf;
 import com.couchbase.client.core.deps.io.netty.util.ReferenceCountUtil;
 import com.couchbase.client.core.error.CouchbaseException;
 import com.couchbase.client.core.error.InvalidArgumentException;
 import com.couchbase.client.core.error.context.ErrorContext;
 import com.couchbase.client.core.error.context.KeyValueErrorContext;
+import com.couchbase.client.core.error.context.SubDocumentErrorContext;
 import com.couchbase.client.core.error.subdoc.DocumentNotJsonException;
 import com.couchbase.client.core.error.subdoc.DocumentTooDeepException;
-import com.couchbase.client.core.error.context.SubDocumentErrorContext;
 import com.couchbase.client.core.error.subdoc.XattrInvalidKeyComboException;
 import com.couchbase.client.core.io.CollectionIdentifier;
 import com.couchbase.client.core.io.netty.kv.KeyValueChannelContext;
 import com.couchbase.client.core.io.netty.kv.MemcacheProtocol;
+import com.couchbase.client.core.io.netty.kv.MemcacheProtocol.FlexibleExtras;
 import com.couchbase.client.core.msg.ResponseStatus;
 import com.couchbase.client.core.retry.RetryStrategy;
-import com.couchbase.client.core.deps.io.netty.buffer.ByteBuf;
-import com.couchbase.client.core.deps.io.netty.buffer.ByteBufAllocator;
-import com.couchbase.client.core.deps.io.netty.buffer.CompositeByteBuf;
 import reactor.util.annotation.Nullable;
 
 import java.time.Duration;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
 
-import static com.couchbase.client.core.io.netty.kv.MemcacheProtocol.*;
+import static com.couchbase.client.core.io.netty.kv.MemcacheProtocol.Status;
+import static com.couchbase.client.core.io.netty.kv.MemcacheProtocol.body;
+import static com.couchbase.client.core.io.netty.kv.MemcacheProtocol.cas;
+import static com.couchbase.client.core.io.netty.kv.MemcacheProtocol.decodeStatus;
+import static com.couchbase.client.core.io.netty.kv.MemcacheProtocol.decodeSubDocumentStatus;
+import static com.couchbase.client.core.io.netty.kv.MemcacheProtocol.mapSubDocumentError;
+import static com.couchbase.client.core.io.netty.kv.MemcacheProtocol.noCas;
+import static com.couchbase.client.core.io.netty.kv.MemcacheProtocol.noDatatype;
+import static com.couchbase.client.core.io.netty.kv.MemcacheProtocol.noExtras;
+import static com.couchbase.client.core.io.netty.kv.MemcacheProtocol.request;
+import static com.couchbase.client.core.io.netty.kv.MemcacheProtocol.status;
 import static java.nio.charset.StandardCharsets.UTF_8;
+import static java.util.Comparator.comparing;
 
 public class SubdocGetRequest extends BaseKeyValueRequest<SubdocGetResponse> {
 
   private static final byte SUBDOC_FLAG_XATTR_PATH = (byte) 0x04;
 
+  private static final Comparator<Command> xattrsFirst = comparing(it -> !it.xattr());
+
   private final byte flags;
   private final List<Command> commands;
   private final String origKey;
+
+  public static SubdocGetRequest create(final Duration timeout, final CoreContext ctx, CollectionIdentifier collectionIdentifier,
+                                        final RetryStrategy retryStrategy, final String key,
+                                        final byte flags, final List<CoreSubdocGetCommand> commands, final RequestSpan span) {
+    return new SubdocGetRequest(timeout, ctx, collectionIdentifier, retryStrategy, key, flags, convertCommands(commands), span);
+  }
 
   public SubdocGetRequest(final Duration timeout, final CoreContext ctx, CollectionIdentifier collectionIdentifier,
                           final RetryStrategy retryStrategy, final String key,
@@ -65,6 +87,23 @@ public class SubdocGetRequest extends BaseKeyValueRequest<SubdocGetResponse> {
     if (span != null) {
       span.attribute(TracingIdentifiers.ATTR_OPERATION, TracingIdentifiers.SPAN_REQUEST_KV_LOOKUP_IN);
     }
+  }
+
+  private static List<Command> convertCommands(List<CoreSubdocGetCommand> commands) {
+    List<SubdocGetRequest.Command> result = new ArrayList<>(commands.size());
+    for (int i = 0, len = commands.size(); i < len; i++) {
+      CoreSubdocGetCommand core = commands.get(i);
+      result.add(new SubdocGetRequest.Command(
+        core.type(),
+        core.path(),
+        core.xattr(),
+        i
+      ));
+    }
+
+    // xattrs must come first. decode() puts the results back in original order.
+    result.sort(xattrsFirst);
+    return result;
   }
 
   @Override

commit 37f457fa3f827a5c3c51681af93fc307c79b6721

JCBC-2062 Infinite recursion in reactive lookupIn
Motivation
----------
A regression in Java SDK 3.4.5, introduced in commit
79df52e4002, causes ReactiveCollection.lookupIn to always
fail with a StackOverflowException.

Modifications
-------------
Modify CoreKvOps.subdocGetReactive so it delegates to
subdocGetAsync instead of calling itself.

Change-Id: I1061f871c0e6a17cff464337939f57d53ea2a3eb
Reviewed-on: https://review.couchbase.org/c/couchbase-jvm-clients/+/190001
Reviewed-by: Michael Reiche <michael.reiche@couchbase.com>
Tested-by: Build Bot <build@couchbase.com>

core-io/src/main/java/com/couchbase/client/core/api/kv/CoreKvOps.java
@@ -321,7 +321,7 @@ public interface CoreKvOps {
       List<CoreSubdocGetCommand> commands,
       boolean accessDeleted
   ) {
-    return Mono.defer(() -> subdocGetReactive(common, key, commands, accessDeleted));
+    return Mono.defer(() -> subdocGetAsync(common, key, commands, accessDeleted).toMono());
   }
 
   Flux<CoreGetResult> getAllReplicasReactive(

java-client/src/integrationTest/java/com/couchbase/client/java/SubdocIntegrationTest.java
@@ -62,6 +62,7 @@ import static java.util.Collections.emptyMap;
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
@@ -578,5 +579,19 @@ class SubdocIntegrationTest extends JavaIntegrationTest {
     // just like `get` or `count`.
     // Java SDK diverges from spec; it treats all errors as "does not exit".
     assertFalse(result.exists(0));
+  }
+
+  @Test
+  void reactiveSmokeTest() {
+    String id = UUID.randomUUID().toString();
+
+    collection.upsert(id, mapOf());
+
+    MutateInResult mutate = collection.reactive().mutateIn(id, listOf(MutateInSpec.upsert("foo", "bar"))).block();
+    assertNotNull(mutate);
+
+    LookupInResult lookup = collection.reactive().lookupIn(id, listOf(LookupInSpec.get("foo"))).block();
+    assertNotNull(lookup);
+    assertEquals("bar", lookup.contentAs(0, String.class));
   }
 }