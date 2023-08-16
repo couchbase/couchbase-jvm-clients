ca4c752e288515921d240bcfc1c0fae00928e8e5
Marcin Grzejszczak <marcin@grzejszczak.pl>
2023-08-16 11:15:24 +0000

    Updated copyright dates

--- tracing-observation/src/integrationTest/java/com/couchbase/client/tracing/observation/ObservationTracingIntegrationTest.java
+++ tracing-observation/src/integrationTest/java/com/couchbase/client/tracing/observation/ObservationTracingIntegrationTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019 Couchbase, Inc.
+ * Copyright (c) 2023 Couchbase, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -55,8 +55,7 @@ class ObservationTracingIntegrationTest extends ClusterAwareIntegrationTest {
   static void beforeEach(ObservationRegistry observationRegistry) {
     TestNodeConfig config = config().firstNodeWith(Services.KV).get();
 
-    cluster = Cluster.connect(
-      String.format("couchbase://%s:%d", config.hostname(), config.ports().get(Services.KV)),
+    cluster = Cluster.connect("couchbase://" + config.hostname(),
       clusterOptions(config().adminUsername(), config().adminPassword())
         .environment(env -> env.requestTracer(ObservationRequestTracer.wrap(observationRegistry)))
     );
