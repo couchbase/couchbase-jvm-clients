b03c24d0d2775202c52393c48b379867000a2e4d
Graham Pople <grahampople@gmail.com>
2023-09-11 15:52:11 +0100

    Scala FIT performer: reformat
    Just reformatting bucket management logic before modifying it.
    
    Change-Id: Ic8f2b0fecaec2c9e9d30942ffc58145733c645f6
    Reviewed-on: https://review.couchbase.org/c/couchbase-jvm-clients/+/196946
    Tested-by: Graham Pople <graham.pople@couchbase.com>
    Reviewed-by: Graham Pople <graham.pople@couchbase.com>

--- scala-fit-performer/src/main/scala/com/couchbase/client/performer/scala/manager/BucketManagerHelper.scala
+++ scala-fit-performer/src/main/scala/com/couchbase/client/performer/scala/manager/BucketManagerHelper.scala
@@ -18,7 +18,10 @@ package com.couchbase.client.performer.scala.manager
 
 // [skip:<1.4.1]
 
-import com.couchbase.client.performer.scala.util.OptionsUtil.{DefaultManagementTimeout, convertDuration}
+import com.couchbase.client.performer.scala.util.OptionsUtil.{
+  DefaultManagementTimeout,
+  convertDuration
+}
 import com.couchbase.client.protocol.run.Result
 import com.couchbase.client.protocol.sdk.Command
 import com.couchbase.client.protocol.sdk.cluster.bucketmanager._
@@ -38,88 +41,109 @@ import scala.util.{Failure, Success}
 
 object BucketManagerHelper {
 
-    def handleBucketManager(cluster: Cluster, command: Command): Result.Builder = {
-        val bm = command.getClusterCommand.getBucketManager
+  def handleBucketManager(cluster: Cluster, command: Command): Result.Builder = {
+    val bm = command.getClusterCommand.getBucketManager
 
-        val result = Result.newBuilder()
+    val result = Result.newBuilder()
 
-        if (bm.hasGetBucket) {
-            val bucketName = bm.getGetBucket.getBucketName
+    if (bm.hasGetBucket) {
+      val bucketName = bm.getGetBucket.getBucketName
 
-            val response = cluster.buckets.getBucket(bucketName,
-                if (bm.getGetBucket.hasOptions && bm.getGetBucket.getOptions.hasTimeoutMsecs) scala.concurrent.duration.Duration(bm.getGetBucket.getOptions.getTimeoutMsecs, TimeUnit.MILLISECONDS)
-                else DefaultManagementTimeout)
+      val response = cluster.buckets.getBucket(
+        bucketName,
+        if (bm.getGetBucket.hasOptions && bm.getGetBucket.getOptions.hasTimeoutMsecs)
+          scala.concurrent.duration
+            .Duration(bm.getGetBucket.getOptions.getTimeoutMsecs, TimeUnit.MILLISECONDS)
+        else DefaultManagementTimeout
+      )
 
-            response match {
-                case Success(bucketSettings: com.couchbase.client.scala.manager.bucket.BucketSettings) => populateResult(result, bucketSettings)
-                case Failure(e) => throw e
-            }
-        } else {
-            throw new UnsupportedOperationException(new IllegalArgumentException("Unknown operation"))
-        }
+      response match {
+        case Success(bucketSettings: com.couchbase.client.scala.manager.bucket.BucketSettings) =>
+          populateResult(result, bucketSettings)
+        case Failure(e) => throw e
+      }
+    } else {
+      throw new UnsupportedOperationException(new IllegalArgumentException("Unknown operation"))
+    }
+  }
+
+  def handleBucketManagerReactive(cluster: ReactiveCluster, command: Command): SMono[Result] = {
+    val bm = command.getClusterCommand.getBucketManager
+
+    val result = Result.newBuilder()
+
+    if (bm.hasGetBucket) {
+      val bucketName = bm.getGetBucket.getBucketName
+
+      val response: SMono[bucket.BucketSettings] = cluster.buckets.getBucket(
+        bucketName,
+        if (bm.getGetBucket.hasOptions && bm.getGetBucket.getOptions.hasTimeoutMsecs)
+          scala.concurrent.duration
+            .Duration(bm.getGetBucket.getOptions.getTimeoutMsecs, TimeUnit.MILLISECONDS)
+        else DefaultManagementTimeout
+      )
+
+      response.map(r => {
+        populateResult(result, r)
+        result.build()
+      })
+    } else {
+      SMono.error(new IllegalArgumentException("Unknown operation"))
+    }
+  }
+
+  def populateResult(
+      result: Result.Builder,
+      response: com.couchbase.client.scala.manager.bucket.BucketSettings
+  ): Result.Builder = {
+    val builder = BucketSettings.newBuilder()
+
+    builder
+      .setName(response.name)
+      .setFlushEnabled(response.flushEnabled)
+      .setRamQuotaMB(response.ramQuotaMB)
+      .setNumReplicas(response.numReplicas)
+      .setReplicaIndexes(response.replicaIndexes)
+      .setMaxExpirySeconds(response.maxTTL.getOrElse(0))
+
+    response.bucketType match {
+      case Couchbase => builder.setBucketType(BucketType.COUCHBASE)
+      case Memcached => builder.setBucketType(BucketType.MEMCACHED)
+      case Ephemeral => builder.setBucketType(BucketType.EPHEMERAL)
     }
 
-    def handleBucketManagerReactive(cluster: ReactiveCluster, command: Command): SMono[Result] = {
-        val bm = command.getClusterCommand.getBucketManager
-
-        val result = Result.newBuilder()
-
-        if (bm.hasGetBucket) {
-            val bucketName = bm.getGetBucket.getBucketName
-
-            val response: SMono[bucket.BucketSettings] = cluster.buckets.getBucket(bucketName,
-                if (bm.getGetBucket.hasOptions && bm.getGetBucket.getOptions.hasTimeoutMsecs) scala.concurrent.duration.Duration(bm.getGetBucket.getOptions.getTimeoutMsecs, TimeUnit.MILLISECONDS)
-                else DefaultManagementTimeout)
-
-            response.map(r => {
-                populateResult(result, r)
-                result.build()
-            })
-        } else {
-            SMono.error(new IllegalArgumentException("Unknown operation"))
-        }
+    response.ejectionMethod match {
+      case EjectionMethod.FullEviction => builder.setEvictionPolicy(EvictionPolicyType.FULL)
+      case EjectionMethod.NoEviction   => builder.setEvictionPolicy(EvictionPolicyType.NO_EVICTION)
+      case EjectionMethod.NotRecentlyUsed =>
+        builder.setEvictionPolicy(EvictionPolicyType.NOT_RECENTLY_USED)
+      case EjectionMethod.ValueOnly => builder.setEvictionPolicy(EvictionPolicyType.VALUE_ONLY)
     }
 
-    def populateResult(result: Result.Builder, response: com.couchbase.client.scala.manager.bucket.BucketSettings): Result.Builder = {
-        val builder = BucketSettings.newBuilder()
-
-        builder.setName(response.name)
-                .setFlushEnabled(response.flushEnabled)
-                .setRamQuotaMB(response.ramQuotaMB)
-                .setNumReplicas(response.numReplicas)
-                .setReplicaIndexes(response.replicaIndexes)
-                .setMaxExpirySeconds(response.maxTTL.getOrElse(0))
-
-        response.bucketType match {
-            case Couchbase => builder.setBucketType(BucketType.COUCHBASE)
-            case Memcached => builder.setBucketType(BucketType.MEMCACHED)
-            case Ephemeral => builder.setBucketType(BucketType.EPHEMERAL)
-        }
-
-        response.ejectionMethod match {
-            case EjectionMethod.FullEviction => builder.setEvictionPolicy(EvictionPolicyType.FULL)
-            case EjectionMethod.NoEviction => builder.setEvictionPolicy(EvictionPolicyType.NO_EVICTION)
-            case EjectionMethod.NotRecentlyUsed => builder.setEvictionPolicy(EvictionPolicyType.NOT_RECENTLY_USED)
-            case EjectionMethod.ValueOnly => builder.setEvictionPolicy(EvictionPolicyType.VALUE_ONLY)
-        }
-
-        response.compressionMode.orNull match {
-            case Off => builder.setCompressionMode(CompressionMode.OFF)
-            case Active => builder.setCompressionMode(CompressionMode.ACTIVE)
-            case _ => builder.setCompressionMode(CompressionMode.PASSIVE)
-        }
-
-        response.minimumDurabilityLevel match {
-            case Disabled => builder.setMinimumDurabilityLevel(Durability.NONE)
-            case ClientVerified(_, _) => builder.setMinimumDurabilityLevel(Durability.NONE)
-            case Majority => builder.setMinimumDurabilityLevel(Durability.MAJORITY)
-            case MajorityAndPersistToActive => builder.setMinimumDurabilityLevel(Durability.MAJORITY_AND_PERSIST_TO_ACTIVE)
-            case PersistToMajority => builder.setMinimumDurabilityLevel(Durability.PERSIST_TO_MAJORITY)
-        }
-
-        result.setSdk(com.couchbase.client.protocol.sdk.Result.newBuilder()
-                .setBucketManagerResult(com.couchbase.client.protocol.sdk.cluster.bucketmanager.Result.newBuilder()
-                        .setBucketSettings(builder)))
+    response.compressionMode.orNull match {
+      case Off    => builder.setCompressionMode(CompressionMode.OFF)
+      case Active => builder.setCompressionMode(CompressionMode.ACTIVE)
+      case _      => builder.setCompressionMode(CompressionMode.PASSIVE)
     }
+
+    response.minimumDurabilityLevel match {
+      case Disabled             => builder.setMinimumDurabilityLevel(Durability.NONE)
+      case ClientVerified(_, _) => builder.setMinimumDurabilityLevel(Durability.NONE)
+      case Majority             => builder.setMinimumDurabilityLevel(Durability.MAJORITY)
+      case MajorityAndPersistToActive =>
+        builder.setMinimumDurabilityLevel(Durability.MAJORITY_AND_PERSIST_TO_ACTIVE)
+      case PersistToMajority => builder.setMinimumDurabilityLevel(Durability.PERSIST_TO_MAJORITY)
+    }
+
+    result.setSdk(
+      com.couchbase.client.protocol.sdk.Result
+        .newBuilder()
+        .setBucketManagerResult(
+          com.couchbase.client.protocol.sdk.cluster.bucketmanager.Result
+            .newBuilder()
+            .setBucketSettings(builder)
+        )
+    )
+  }
 
 }
