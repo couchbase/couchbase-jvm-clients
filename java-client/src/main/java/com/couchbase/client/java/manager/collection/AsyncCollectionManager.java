couchbase/couchbase-jvm-clients

commit 31aaf03329cdbdd2e8bff6455852689df6957f43

JCBC-2089 Add Support for Bucket History (nodedup) Setting.
two new updateCollection() methods that take args

scope, collection, settings
scope, collection, settings, options

There are also two new updateCollection() methods that take args

scope, collection, settings
scope, collection, settings, options.

And two new dropCollections() methods that take args

scope, collection
scope, collection, options

I've also changed the tests to use the new APIs.

And additions to Fit Performer BucketManagerHelper to test the new methods.
Also addition to Fit Performer CollectionManagerHelper for getAllScopes()

The existing BucketSettings cannot be used because it has unboxed fields,
so it is not possible to have them 'not set'.

Change-Id: Ic6599e426f59530b6cb7a56feee8cc8337843993
Reviewed-on: https://review.couchbase.org/c/couchbase-jvm-clients/+/197346
Reviewed-by: David Nault <david.nault@couchbase.com>
Tested-by: Build Bot <build@couchbase.com>

java-client/src/main/java/com/couchbase/client/java/manager/collection/AsyncCollectionManager.java
@@ -36,12 +36,14 @@ import java.util.stream.Collectors;
 
 import static com.couchbase.client.core.util.Validators.notNull;
 import static com.couchbase.client.core.util.Validators.notNullOrEmpty;
+import static com.couchbase.client.java.manager.collection.CreateCollectionSettings.createCollectionSettings;
 import static com.couchbase.client.java.manager.collection.CreateCollectionOptions.createCollectionOptions;
 import static com.couchbase.client.java.manager.collection.CreateScopeOptions.createScopeOptions;
 import static com.couchbase.client.java.manager.collection.DropCollectionOptions.dropCollectionOptions;
 import static com.couchbase.client.java.manager.collection.DropScopeOptions.dropScopeOptions;
 import static com.couchbase.client.java.manager.collection.GetAllScopesOptions.getAllScopesOptions;
 import static com.couchbase.client.java.manager.collection.GetScopeOptions.getScopeOptions;
+import static com.couchbase.client.java.manager.collection.UpdateCollectionOptions.updateCollectionOptions;
 import static java.util.Objects.requireNonNull;
 
 /**
@@ -84,8 +86,9 @@ public class AsyncCollectionManager {
    * @throws ScopeNotFoundException (async) if the specified scope does not exist.
    * @throws CouchbaseException (async) if any other generic unhandled/unexpected errors.
    */
+  @Deprecated
   public CompletableFuture<Void> createCollection(final CollectionSpec collectionSpec) {
-    return createCollection(collectionSpec, createCollectionOptions());
+    return createCollection(collectionSpec.scopeName(), collectionSpec.name(), CreateCollectionSettings.createCollectionSettings());
   }
 
   /**
@@ -100,8 +103,9 @@ public class AsyncCollectionManager {
    * @throws ScopeNotFoundException (async) if the specified scope does not exist.
    * @throws CouchbaseException (async) if any other generic unhandled/unexpected errors.
    */
+  @Deprecated
   public CompletableFuture<Void> createCollection(final CollectionSpec collectionSpec,
-      final CreateCollectionOptions options) {
+                                                  final CreateCollectionOptions options) {
     return coreCollectionManager.createCollection(collectionSpec.scopeName(), collectionSpec.name(),
       new CoreCreateOrUpdateCollectionSettings() {
         @Override
@@ -111,9 +115,46 @@ public class AsyncCollectionManager {
 
         @Override
         public Boolean history() {
-          return null;
+          return collectionSpec.history();
         }
       }, options.build());
+  }
+
+  /**
+   * Creates a collection if it does not already exist with custom options.
+   * <p>
+   * Note that a scope needs to be created first (via {@link #createScope(String)}) if it doesn't exist already.
+   *
+   * @param scopeName name of scope to create collection in
+   * @param collectionName name of collection to create
+   * @param settings the collection settings
+   * @return a {@link CompletableFuture} completing when the operation is applied or failed with an error.
+   * @throws CollectionExistsException (async) if the collection already exists
+   * @throws ScopeNotFoundException (async) if the specified scope does not exist.
+   * @throws CouchbaseException (async) if any other generic unhandled/unexpected errors.
+   */
+  @Stability.Volatile
+  public CompletableFuture<Void> createCollection(String scopeName, String collectionName, CreateCollectionSettings settings) {
+    return coreCollectionManager.createCollection(scopeName, collectionName, settings.build(), createCollectionOptions().build());
+  }
+
+  /**
+   * Creates a collection if it does not already exist with custom options.
+   * <p>
+   * Note that a scope needs to be created first (via {@link #createScope(String)}) if it doesn't exist already.
+   *
+   * @param scopeName name of scope to create collection in
+   * @param collectionName name of collection to create
+   * @param settings the collection settings
+   * @param options the custom options to apply.
+   * @return a {@link CompletableFuture} completing when the operation is applied or failed with an error.
+   * @throws CollectionExistsException (async) if the collection already exists
+   * @throws ScopeNotFoundException (async) if the specified scope does not exist.
+   * @throws CouchbaseException (async) if any other generic unhandled/unexpected errors.
+   */
+  @Stability.Volatile
+  public CompletableFuture<Void> createCollection(String scopeName, String collectionName, CreateCollectionSettings settings, CreateCollectionOptions options) {
+    return coreCollectionManager.createCollection(scopeName, collectionName, settings.build(), options.build());
   }
 
   /**
@@ -142,14 +183,47 @@ public class AsyncCollectionManager {
   }
 
   /**
+   * Updates a collection with custom options.
+   *
+   * @param scopeName name of scope to update collection in
+   * @param collectionName name of collection to update
+   * @param settings the collection settings
+   * @return a {@link CompletableFuture} completing when the operation is applied or failed with an error.
+   * @throws ScopeNotFoundException (async) if the specified scope does not exist.
+   * @throws CouchbaseException (async) if any other generic unhandled/unexpected errors.
+   */
+  @Stability.Volatile
+  public CompletableFuture<Void> updateCollection(final String scopeName, final String collectionName, final UpdateCollectionSettings settings) {
+    return updateCollection(scopeName, collectionName, settings, updateCollectionOptions());
+  }
+
+  /**
+   * Updates a collection with custom options.
+   *
+   * @param scopeName name of scope to update collection in
+   * @param collectionName name of collection to update
+   * @param settings the collection settings
+   * @param options the custom options to apply.
+   * @return a {@link CompletableFuture} completing when the operation is applied or failed with an error.
+   * @throws CollectionNotFoundException (async) if the collection does not exist.
+   * @throws ScopeNotFoundException (async) if the specified scope does not exist.
+   * @throws CouchbaseException (async) if any other generic unhandled/unexpected errors.
+   */
+  @Stability.Volatile
+  public CompletableFuture<Void> updateCollection(final String scopeName, final String collectionName, final UpdateCollectionSettings settings, final UpdateCollectionOptions options) {
+    return coreCollectionManager.updateCollection(scopeName, collectionName, settings.build(), options.build());
+  }
+
+  /**
    * Drops a collection if it exists.
    *
    * @param collectionSpec the collection spec that contains the properties of the collection.
    * @return a {@link CompletableFuture} completing when the operation is applied or failed with an error.
-   * @throws CollectionNotFoundException (async) if the collection did not exist.
+   * @throws CollectionNotFoundException (async) if the collection does not exist.
    * @throws ScopeNotFoundException (async) if the specified scope does not exist.
    * @throws CouchbaseException (async) if any other generic unhandled/unexpected errors.
    */
+  @Deprecated
   public CompletableFuture<Void> dropCollection(final CollectionSpec collectionSpec) {
     return dropCollection(collectionSpec, dropCollectionOptions());
   }
@@ -164,9 +238,44 @@ public class AsyncCollectionManager {
    * @throws ScopeNotFoundException (async) if the specified scope does not exist.
    * @throws CouchbaseException (async) if any other generic unhandled/unexpected errors.
    */
+  @Deprecated
   public CompletableFuture<Void> dropCollection(final CollectionSpec collectionSpec,
-      final DropCollectionOptions options) {
+                                                final DropCollectionOptions options) {
     return coreCollectionManager.dropCollection(collectionSpec.scopeName(), collectionSpec.name(), options.build());
+  }
+
+  /**
+   * Drops a collection if it exists.
+   *
+   * @param scopeName name of scope to drop collection from
+   * @param collectionName name of collection to drop
+   * @return a {@link CompletableFuture} completing when the operation is applied or failed with an error.
+   * @throws CollectionNotFoundException (async) if the collection did not exist.
+   * @throws ScopeNotFoundException (async) if the specified scope does not exist.
+   * @throws CouchbaseException (async) if any other generic unhandled/unexpected errors.
+   */
+  @Stability.Volatile
+  public CompletableFuture<Void> dropCollection(final String scopeName,
+                                                final String collectionName) {
+    return dropCollection(scopeName, collectionName, dropCollectionOptions());
+  }
+
+  /**
+   * Drops a collection if it exists with custom options.
+   *
+   * @param scopeName name of scope to drop collection from
+   * @param collectionName name of collection to drop
+   * @param options the custom options to apply.
+   * @return a {@link CompletableFuture} completing when the operation is applied or failed with an error.
+   * @throws CollectionNotFoundException (async) if the collection did not exist.
+   * @throws ScopeNotFoundException (async) if the specified scope does not exist.
+   * @throws CouchbaseException (async) if any other generic unhandled/unexpected errors.
+   */
+  @Stability.Volatile
+  public CompletableFuture<Void> dropCollection(final String scopeName,
+                                                final String collectionName,
+                                                final DropCollectionOptions options) {
+    return coreCollectionManager.dropCollection(scopeName, collectionName, options.build());
   }
 
   /**
@@ -262,7 +371,7 @@ public class AsyncCollectionManager {
         .thenApply(manifest -> manifest.scopes().stream()
             .map(s -> ScopeSpec.create(s.name(),
                 s.collections().stream()
-                    .map(c -> CollectionSpec.create(c.name(), s.name(), c.maxExpiry() == null ? Duration.ZERO : Duration.ofSeconds(c.maxExpiry())))
+                    .map(c -> CollectionSpec.internalCreate(c.name(), s.name(), c.maxExpiry() == null ? Duration.ZERO : Duration.ofSeconds(c.maxExpiry()), c.history()))
                     .collect(Collectors.toSet())))
             .collect(Collectors.toList()));
   }

java-client/src/integrationTest/java/com/couchbase/client/java/AnalyticsCollectionIntegrationTest.java
@@ -43,7 +43,6 @@ import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.time.Duration;
 import java.util.List;
 import java.util.Set;
 import java.util.UUID;
@@ -105,7 +104,7 @@ class AnalyticsCollectionIntegrationTest extends JavaIntegrationTest {
     collectionManager.createScope(scopeName);
     ConsistencyUtil.waitUntilScopePresent(cluster.core(), bucket.name(), scopeName);
     CollectionSpec collSpec = CollectionSpec.create(collectionName, scopeName);
-    collectionManager.createCollection(collSpec);
+    collectionManager.createCollection(scopeName, collectionName);
     ConsistencyUtil.waitUntilCollectionPresent(cluster.core(), bucket.name(), collSpec.scopeName(), collSpec.name());
     waitUntilCondition(() -> collectionExists(collectionManager, collSpec));
 

java-client/src/integrationTest/java/com/couchbase/client/java/KeyValueCollectionIntegrationTest.java
@@ -16,7 +16,6 @@
 package com.couchbase.client.java;
 
 import com.couchbase.client.core.error.UnambiguousTimeoutException;
-import com.couchbase.client.core.io.CollectionIdentifier;
 import com.couchbase.client.core.retry.RetryReason;
 import com.couchbase.client.core.util.ConsistencyUtil;
 import com.couchbase.client.java.kv.GetResult;
@@ -33,6 +32,7 @@ import org.junit.jupiter.api.Test;
 import java.time.Duration;
 import java.util.UUID;
 
+import static com.couchbase.client.core.io.CollectionIdentifier.DEFAULT_SCOPE;
 import static com.couchbase.client.java.kv.GetOptions.getOptions;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotSame;
@@ -62,7 +62,7 @@ public class KeyValueCollectionIntegrationTest extends JavaIntegrationTest {
   @IgnoreWhen(clusterTypes = ClusterType.CAVES, isProtostellarWillWorkLater = true) // Fails as CNG is not yet waiting for the collection to be ready before continuing
   void recognizesCollectionAfterCreation() {
     String collId = UUID.randomUUID().toString().substring(0, 10);
-    CollectionSpec collectionSpec = CollectionSpec.create(collId, CollectionIdentifier.DEFAULT_SCOPE);
+    CollectionSpec collectionSpec = CollectionSpec.create(collId, DEFAULT_SCOPE);
     bucket.collections().createCollection(collectionSpec);
     if (!config().isProtostellar()) ConsistencyUtil.waitUntilCollectionPresent(cluster.core(), bucket.name(), collectionSpec.scopeName(), collectionSpec.name());
 

java-client/src/integrationTest/java/com/couchbase/client/java/QueryCollectionIntegrationTest.java
@@ -16,9 +16,6 @@
 
 package com.couchbase.client.java;
 
-import com.couchbase.client.core.error.IndexExistsException;
-import com.couchbase.client.core.error.QueryException;
-import com.couchbase.client.core.json.Mapper;
 import com.couchbase.client.core.service.ServiceType;
 import com.couchbase.client.core.util.ConsistencyUtil;
 import com.couchbase.client.java.json.JsonArray;
@@ -42,17 +39,9 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.time.Duration;
-import java.util.LinkedHashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.UUID;
-import java.util.concurrent.CompletableFuture;
-import java.util.function.Function;
-import java.util.function.Predicate;
 
-import static com.couchbase.client.core.util.CbThrowables.hasCause;
-import static com.couchbase.client.core.util.CbThrowables.throwIfUnchecked;
-import static com.couchbase.client.java.AsyncUtils.block;
 import static com.couchbase.client.java.manager.query.QueryIndexManagerIntegrationTest.DISABLE_QUERY_TESTS_FOR_CLUSTER;
 import static com.couchbase.client.java.manager.query.QueryIndexManagerIntegrationTest.REQUIRE_MB_50132;
 import static com.couchbase.client.java.query.QueryOptions.queryOptions;
@@ -101,7 +90,7 @@ class QueryCollectionIntegrationTest extends JavaIntegrationTest {
     if (!config().isProtostellar()) ConsistencyUtil.waitUntilScopePresent(cluster.core(), bucket.name(), SCOPE_NAME);
     waitUntilCondition(() -> scopeExists(collectionManager, SCOPE_NAME));
 
-    collectionManager.createCollection(collSpec);
+    collectionManager.createCollection(SCOPE_NAME, COLLECTION_NAME);
     if (!config().isProtostellar()) ConsistencyUtil.waitUntilCollectionPresent(cluster.core(), bucket.name(), collSpec.scopeName(), collSpec.name());
     waitUntilCondition(() -> collectionExists(collectionManager, collSpec));
 

java-client/src/integrationTest/java/com/couchbase/client/java/RateLimitingIntegrationTest.java
@@ -52,9 +52,7 @@ import com.couchbase.client.test.Flaky;
 import com.couchbase.client.test.IgnoreWhen;
 import com.couchbase.client.test.Util;
 import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -249,7 +247,7 @@ class RateLimitingIntegrationTest extends JavaIntegrationTest {
     createLimitedScope(scopeName, config().bucketname(), limits);
 
     CollectionManager collectionManager = adminCluster.bucket(config().bucketname()).collections();
-    collectionManager.createCollection(CollectionSpec.create(scopeName, scopeName));
+    collectionManager.createCollection(scopeName, scopeName);
     ConsistencyUtil.waitUntilCollectionPresent(adminCluster.core(), config().bucketname(), scopeName, scopeName);
 
     Collection collection = adminCluster.bucket(config().bucketname()).scope(scopeName).collection(scopeName);
@@ -423,11 +421,11 @@ class RateLimitingIntegrationTest extends JavaIntegrationTest {
     CollectionManager collectionManager = adminCluster.bucket(config().bucketname()).collections();
 
     try {
-      collectionManager.createCollection(CollectionSpec.create("collection1", scopeName));
+      collectionManager.createCollection(scopeName, "collection1");
       ConsistencyUtil.waitUntilCollectionPresent(adminCluster.core(), config().bucketname(), scopeName, "collection1");
 
       assertThrows(QuotaLimitedException.class, () ->
-        collectionManager.createCollection(CollectionSpec.create("collection2", scopeName)));
+        collectionManager.createCollection(scopeName, "collection2"));
     } finally {
       collectionManager.dropScope(scopeName);
     }
@@ -627,18 +625,16 @@ class RateLimitingIntegrationTest extends JavaIntegrationTest {
       ));
 
     try {
-      CollectionSpec collectionSpec = CollectionSpec.create(collectionName, scopeName);
-      collectionManager.createCollection(collectionSpec);
-      ConsistencyUtil.waitUntilCollectionPresent(adminCluster.core(), config().bucketname(), collectionSpec.scopeName(), collectionSpec.name());
-      waitUntilCondition(() -> collectionExists(collectionManager, collectionSpec));
+      collectionManager.createCollection(scopeName, collectionName);
+      ConsistencyUtil.waitUntilCollectionPresent(adminCluster.core(), config().bucketname(), scopeName, collectionName);
+      waitUntilCondition(() -> collectionExists(collectionManager, CollectionSpec.create(collectionName, scopeName)));
 
       waitForService(adminCluster.bucket(config().bucketname()), ServiceType.SEARCH);
       Util.waitUntilCondition(() -> {
         try {
           adminCluster.searchIndexes().upsertIndex(new SearchIndex("ratelimits1", config().bucketname())
-                  .params(params));
-        }
-        catch (CouchbaseException err) {
+            .params(params));
+        } catch (CouchbaseException err) {
           // See intermittent failures on CI:
           // Unknown search error: {"error":"rest_create_index: error creating index: ratelimits1, err: manager_api: CreateIndex, Prepare failed, err: collection_utils: collection: 'searchCollection' doesn't belong to scope: 'ratelimitSearch' in bucket: "...
           LOGGER.info("Got error upserting index: {}", err.toString());

java-client/src/integrationTest/java/com/couchbase/client/java/manager/collection/CollectionManagerErrorIntegrationTest.java
@@ -16,7 +16,6 @@
 
 package com.couchbase.client.java.manager.collection;
 
-import com.couchbase.client.core.env.IoConfig;
 import com.couchbase.client.core.error.FeatureNotAvailableException;
 import com.couchbase.client.core.service.ServiceType;
 import com.couchbase.client.java.Bucket;
@@ -31,7 +30,6 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import java.time.Duration;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -43,7 +41,7 @@ public class CollectionManagerErrorIntegrationTest extends JavaIntegrationTest {
 
   @BeforeAll
   static void setup() {
-    cluster = createCluster(env -> env.ioConfig(IoConfig.captureTraffic(ServiceType.MANAGER)));
+    cluster = createCluster(env -> env.ioConfig(io -> io.captureTraffic(ServiceType.MANAGER)));
     Bucket bucket = cluster.bucket(config().bucketname());
     collections = bucket.collections();
     bucket.waitUntilReady(WAIT_UNTIL_READY_DEFAULT);
@@ -66,11 +64,11 @@ public class CollectionManagerErrorIntegrationTest extends JavaIntegrationTest {
     assertThrows(FeatureNotAvailableException.class, () -> collections.dropScope("foo"));
     assertThrows(
       FeatureNotAvailableException.class,
-      () -> collections.dropCollection(CollectionSpec.create("foo", "bar"))
+      () -> collections.dropCollection("bar", "foo")
     );
     assertThrows(
       FeatureNotAvailableException.class,
-      () -> collections.createCollection(CollectionSpec.create("foo", "bar"))
+      () -> collections.createCollection("bar", "foo")
     );
   }
 
@@ -89,9 +87,9 @@ public class CollectionManagerErrorIntegrationTest extends JavaIntegrationTest {
 
       CollectionManager mgr = bucket.collections();
       assertThrows(FeatureNotAvailableException.class, () -> mgr.createScope("a"));
-      assertThrows(FeatureNotAvailableException.class, () -> mgr.createCollection(CollectionSpec.create("a", "b")));
+      assertThrows(FeatureNotAvailableException.class, () -> mgr.createCollection("b", "a"));
       assertThrows(FeatureNotAvailableException.class, () -> mgr.dropScope("a"));
-      assertThrows(FeatureNotAvailableException.class, () -> mgr.dropCollection(CollectionSpec.create("a", "b")));
+      assertThrows(FeatureNotAvailableException.class, () -> mgr.dropCollection("b", "a"));
     } finally {
       cluster.buckets().dropBucket(bucketName);
     }

commit 82f7681d88d27a78e4b58b1e21adc4e372ded417

JCBC-2089 (Followup) Fix unreleased regression
Motivation
----------
An unreleased regression in 31aaf03329c caused
AsyncCollectionManager.createCollection(CollectionSpec)
to ignore the maxExpiry setting.

Modifications
-------------
Make sure maxExpiry is still propagated when creating a
collection using a deprecated method.

Deliberately omit the "history" property. This sidesteps
the issue of the server rejecting `history=false` for
non-magma buckets (see MB-59144).

Change-Id: If71cfc07d552b6695759d8c0126c1595b8941847
Reviewed-on: https://review.couchbase.org/c/couchbase-jvm-clients/+/198975
Reviewed-by: Michael Reiche <michael.reiche@couchbase.com>
Tested-by: Build Bot <build@couchbase.com>

java-client/src/main/java/com/couchbase/client/java/manager/collection/AsyncCollectionManager.java
@@ -23,7 +23,6 @@ import com.couchbase.client.core.error.CouchbaseException;
 import com.couchbase.client.core.error.ScopeExistsException;
 import com.couchbase.client.core.error.ScopeNotFoundException;
 import com.couchbase.client.core.manager.CoreCollectionManager;
-import com.couchbase.client.core.manager.collection.CoreCreateOrUpdateCollectionSettings;
 import com.couchbase.client.core.util.PreventsGarbageCollection;
 import com.couchbase.client.java.AsyncBucket;
 import com.couchbase.client.java.AsyncCluster;
@@ -36,8 +35,8 @@ import java.util.stream.Collectors;
 
 import static com.couchbase.client.core.util.Validators.notNull;
 import static com.couchbase.client.core.util.Validators.notNullOrEmpty;
-import static com.couchbase.client.java.manager.collection.CreateCollectionSettings.createCollectionSettings;
 import static com.couchbase.client.java.manager.collection.CreateCollectionOptions.createCollectionOptions;
+import static com.couchbase.client.java.manager.collection.CreateCollectionSettings.createCollectionSettings;
 import static com.couchbase.client.java.manager.collection.CreateScopeOptions.createScopeOptions;
 import static com.couchbase.client.java.manager.collection.DropCollectionOptions.dropCollectionOptions;
 import static com.couchbase.client.java.manager.collection.DropScopeOptions.dropScopeOptions;
@@ -90,7 +89,7 @@ public class AsyncCollectionManager {
    */
   @Deprecated
   public CompletableFuture<Void> createCollection(final CollectionSpec collectionSpec) {
-    return createCollection(collectionSpec.scopeName(), collectionSpec.name(), CreateCollectionSettings.createCollectionSettings());
+    return createCollection(collectionSpec, createCollectionOptions());
   }
 
   /**
@@ -110,18 +109,13 @@ public class AsyncCollectionManager {
   @Deprecated
   public CompletableFuture<Void> createCollection(final CollectionSpec collectionSpec,
                                                   final CreateCollectionOptions options) {
-    return coreCollectionManager.createCollection(collectionSpec.scopeName(), collectionSpec.name(),
-      new CoreCreateOrUpdateCollectionSettings() {
-        @Override
-        public Duration maxExpiry() {
-          return collectionSpec.maxExpiry();
-        }
-
-        @Override
-        public Boolean history() {
-          return collectionSpec.history();
-        }
-      }, options.build());
+    return createCollection(
+      collectionSpec.scopeName(),
+      collectionSpec.name(),
+      createCollectionSettings()
+        .maxExpiry(collectionSpec.maxExpiry()),
+      options
+    );
   }
 
   /**