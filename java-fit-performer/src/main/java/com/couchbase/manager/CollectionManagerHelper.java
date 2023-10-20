43a87da3b5cc8f09ae76031759cf58aeaea974b3
Graham Pople <grahampople@gmail.com>
2023-10-20 14:18:39 +0100

    JCBC-2105: Performer missing most CollectionManager functionality
    Change-Id: Ibc293e5528969f66f62d37c87827017a4d863b9c
    Reviewed-on: https://review.couchbase.org/c/couchbase-jvm-clients/+/199171
    Reviewed-by: Graham Pople <graham.pople@couchbase.com>
    Tested-by: Graham Pople <graham.pople@couchbase.com>

--- java-fit-performer/src/main/java/com/couchbase/manager/CollectionManagerHelper.java
+++ java-fit-performer/src/main/java/com/couchbase/manager/CollectionManagerHelper.java
@@ -16,22 +16,31 @@
 
 package com.couchbase.manager;
 
-// [skip:<3.4.11]
+// [skip:<3.4.12]
 
 import com.couchbase.client.core.cnc.RequestSpan;
 import com.couchbase.client.java.Cluster;
 import com.couchbase.client.java.ReactiveCluster;
 import com.couchbase.client.protocol.run.Result;
 import com.couchbase.client.protocol.sdk.bucket.collectionmanager.CollectionSpec;
+import com.couchbase.client.protocol.sdk.bucket.collectionmanager.CreateCollectionOptions;
+import com.couchbase.client.protocol.sdk.bucket.collectionmanager.CreateCollectionSettings;
+import com.couchbase.client.protocol.sdk.bucket.collectionmanager.CreateScopeOptions;
+import com.couchbase.client.protocol.sdk.bucket.collectionmanager.DropCollectionOptions;
+import com.couchbase.client.protocol.sdk.bucket.collectionmanager.DropScopeOptions;
 import com.couchbase.client.protocol.sdk.bucket.collectionmanager.GetAllScopesOptions;
 import com.couchbase.client.protocol.sdk.bucket.collectionmanager.GetAllScopesResult;
 import com.couchbase.client.protocol.sdk.bucket.collectionmanager.ScopeSpec;
+import com.couchbase.client.protocol.sdk.bucket.collectionmanager.UpdateCollectionOptions;
+import com.couchbase.client.protocol.sdk.bucket.collectionmanager.UpdateCollectionSettings;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.stream.Collectors;
+
+import static com.couchbase.JavaSdkCommandExecutor.setSuccess;
 
 
 public class CollectionManagerHelper {
@@ -46,17 +55,67 @@ public class CollectionManagerHelper {
                                              Result.Builder result) {
 
     var cm = command.getBucketCommand().getCollectionManager();
+    var bucketName = command.getBucketCommand().getBucketName();
+    var collections = cluster.bucket(bucketName).collections();
     if (cm.hasGetAllScopes()) {
       var request = cm.getGetAllScopes();
-      var bucketName = command.getBucketCommand().getBucketName();
       List<com.couchbase.client.java.manager.collection.ScopeSpec> response;
       if (!request.hasOptions()) {
-        response = cluster.bucket(bucketName).collections().getAllScopes();
+        response = collections.getAllScopes();
       } else {
         var options = createGetAllScopesOptions(request.getOptions(), spans);
-        response = cluster.bucket(bucketName).collections().getAllScopes(options);
+        response = collections.getAllScopes(options);
       }
       populateResult(result, response);
+    } else if (cm.hasCreateScope()) {
+      var request = cm.getCreateScope();
+      if (request.hasOptions()) {
+        var options = createScopeOptions(request.getOptions(), spans);
+        collections.createScope(request.getName(), options);
+      } else {
+        collections.createScope(request.getName());
+      }
+      setSuccess(result);
+    } else if (cm.hasDropScope()) {
+      var request = cm.getDropScope();
+      if (request.hasOptions()) {
+        var options = dropScopeOptions(request.getOptions(), spans);
+        collections.dropScope(request.getName(), options);
+      } else {
+        collections.dropScope(request.getName());
+      }
+      setSuccess(result);
+    } else if (cm.hasCreateCollection()) {
+      var request = cm.getCreateCollection();
+      var settings = createCollectionSettings(request.hasSettings(), request.getSettings());
+      if (request.hasOptions()) {
+        var options = createCollectionOptions(request.getOptions(), spans);
+        collections.createCollection(request.getScopeName(), request.getName(), settings, options);
+      } else {
+        collections.createCollection(request.getScopeName(), request.getName(), settings);
+      }
+      setSuccess(result);
+    } else if (cm.hasUpdateCollection()) {
+      var request = cm.getUpdateCollection();
+      var settings = updateCollectionSettings(request.hasSettings(), request.getSettings());
+      if (request.hasOptions()) {
+        var options = updateCollectionOptions(request.getOptions(), spans);
+        collections.updateCollection(request.getScopeName(), request.getName(), settings, options);
+      } else {
+        collections.updateCollection(request.getScopeName(), request.getName(), settings);
+      }
+      setSuccess(result);
+    } else if (cm.hasDropCollection()) {
+      var request = cm.getDropCollection();
+      if (request.hasOptions()) {
+        var options = dropCollectionOptions(request.getOptions(), spans);
+        collections.dropCollection(request.getScopeName(), request.getName(), options);
+      } else {
+        collections.dropCollection(request.getScopeName(), request.getName());
+      }
+      setSuccess(result);
+    } else {
+      throw new UnsupportedOperationException("Unknown CollectionManager operation");
     }
   }
 
@@ -67,10 +126,11 @@ public class CollectionManagerHelper {
 
 
     var cm = command.getBucketCommand().getCollectionManager();
+    var bucketName = command.getBucketCommand().getBucketName();
+    var collections = cluster.bucket(bucketName).collections();
     if (cm.hasGetAllScopes()) {
       Flux<com.couchbase.client.java.manager.collection.ScopeSpec> response;
       var request = cm.getGetAllScopes();
-      var bucketName = command.getBucketCommand().getBucketName();
       if (!request.hasOptions()) {
         response = cluster.bucket(bucketName).collections().getAllScopes();
       } else {
@@ -82,11 +142,78 @@ public class CollectionManagerHelper {
         return result.build();
       });
       return f.last();
+    } else if (cm.hasCreateScope()) {
+      var request = cm.getCreateScope();
+      Mono<Void> r;
+      if (request.hasOptions()) {
+        var options = createScopeOptions(request.getOptions(), spans);
+        r = collections.createScope(request.getName(), options);
+      } else {
+        r = collections.createScope(request.getName());
+      }
+      return r.then(Mono.fromSupplier(() -> {
+        setSuccess(result);
+        return result.build();
+      }));
+    } else if (cm.hasDropScope()) {
+      var request = cm.getDropScope();
+      Mono<Void> r;
+      if (request.hasOptions()) {
+        var options = dropScopeOptions(request.getOptions(), spans);
+        r = collections.dropScope(request.getName(), options);
+      } else {
+        r = collections.dropScope(request.getName());
+      }
+      return r.then(Mono.fromSupplier(() -> {
+        setSuccess(result);
+        return result.build();
+      }));
+    } else if (cm.hasCreateCollection()) {
+      var request = cm.getCreateCollection();
+      var settings = createCollectionSettings(request.hasSettings(), request.getSettings());
+      Mono<Void> r;
+      if (request.hasOptions()) {
+        var options = createCollectionOptions(request.getOptions(), spans);
+        r = collections.createCollection(request.getScopeName(), request.getName(), settings, options);
+      } else {
+        r = collections.createCollection(request.getScopeName(), request.getName(), settings);
+      }
+      return r.then(Mono.fromSupplier(() -> {
+        setSuccess(result);
+        return result.build();
+      }));
+    } else if (cm.hasUpdateCollection()) {
+      var request = cm.getUpdateCollection();
+      var settings = updateCollectionSettings(request.hasSettings(), request.getSettings());
+      Mono<Void> r;
+      if (request.hasOptions()) {
+        var options = updateCollectionOptions(request.getOptions(), spans);
+        r = collections.updateCollection(request.getScopeName(), request.getName(), settings, options);
+      } else {
+        r = collections.updateCollection(request.getScopeName(), request.getName(), settings);
+      }
+      return r.then(Mono.fromSupplier(() -> {
+        setSuccess(result);
+        return result.build();
+      }));
+    } else if (cm.hasDropCollection()) {
+      var request = cm.getDropCollection();
+      Mono<Void> r;
+      if (request.hasOptions()) {
+        var options = dropCollectionOptions(request.getOptions(), spans);
+        r = collections.dropCollection(request.getScopeName(), request.getName(), options);
+      } else {
+        r = collections.dropCollection(request.getScopeName(), request.getName());
+      }
+      return r.then(Mono.fromSupplier(() -> {
+        setSuccess(result);
+        return result.build();
+      }));
     } else {
       return Mono.error(new UnsupportedOperationException(new IllegalArgumentException("Unknown operation")));
     }
-
   }
+
 
   public static void populateResult(Result.Builder result, List<com.couchbase.client.java.manager.collection.ScopeSpec> response) {
     var builder = GetAllScopesResult.newBuilder();
@@ -145,6 +272,73 @@ public class CollectionManagerHelper {
     if (getAllScopesOptions.hasParentSpanId()) options.parentSpan(spans.get(getAllScopesOptions.getParentSpanId()));
 
     return options;
+  }
+
+  private static com.couchbase.client.java.manager.collection.CreateScopeOptions createScopeOptions(CreateScopeOptions options, ConcurrentHashMap<String, RequestSpan> spans) {
+    var out = com.couchbase.client.java.manager.collection.CreateScopeOptions.createScopeOptions();
+
+    if (options.hasTimeoutMsecs()) out.timeout(java.time.Duration.ofMillis(options.getTimeoutMsecs()));
+    if (options.hasParentSpanId()) out.parentSpan(spans.get(options.getParentSpanId()));
+
+    return out;
+  }
+
+  private static com.couchbase.client.java.manager.collection.DropScopeOptions dropScopeOptions(DropScopeOptions options, ConcurrentHashMap<String, RequestSpan> spans) {
+    var out = com.couchbase.client.java.manager.collection.DropScopeOptions.dropScopeOptions();
+
+    if (options.hasTimeoutMsecs()) out.timeout(java.time.Duration.ofMillis(options.getTimeoutMsecs()));
+    if (options.hasParentSpanId()) out.parentSpan(spans.get(options.getParentSpanId()));
+
+    return out;
+  }
+
+  private static com.couchbase.client.java.manager.collection.CreateCollectionOptions createCollectionOptions(CreateCollectionOptions options, ConcurrentHashMap<String, RequestSpan> spans) {
+    var out = com.couchbase.client.java.manager.collection.CreateCollectionOptions.createCollectionOptions();
+
+    if (options.hasTimeoutMsecs()) out.timeout(java.time.Duration.ofMillis(options.getTimeoutMsecs()));
+    if (options.hasParentSpanId()) out.parentSpan(spans.get(options.getParentSpanId()));
+
+    return out;
+  }
+
+  private static com.couchbase.client.java.manager.collection.CreateCollectionSettings createCollectionSettings(boolean hasSettings, CreateCollectionSettings settings) {
+    var out = com.couchbase.client.java.manager.collection.CreateCollectionSettings.createCollectionSettings();
+
+    if (hasSettings) {
+      if (settings.hasExpirySecs()) out.maxExpiry(java.time.Duration.ofSeconds(settings.getExpirySecs()));
+      if (settings.hasHistory()) out.history(settings.getHistory());
+    }
+
+    return out;
+  }
+
+  private static com.couchbase.client.java.manager.collection.UpdateCollectionOptions updateCollectionOptions(UpdateCollectionOptions options, ConcurrentHashMap<String, RequestSpan> spans) {
+    var out = com.couchbase.client.java.manager.collection.UpdateCollectionOptions.updateCollectionOptions();
+
+    if (options.hasTimeoutMsecs()) out.timeout(java.time.Duration.ofMillis(options.getTimeoutMsecs()));
+    if (options.hasParentSpanId()) out.parentSpan(spans.get(options.getParentSpanId()));
+
+    return out;
+  }
+
+  private static com.couchbase.client.java.manager.collection.UpdateCollectionSettings updateCollectionSettings(boolean hasSettings, UpdateCollectionSettings settings) {
+    var out = com.couchbase.client.java.manager.collection.UpdateCollectionSettings.updateCollectionSettings();
+
+    if (hasSettings) {
+      if (settings.hasExpirySecs()) out.maxExpiry(java.time.Duration.ofSeconds(settings.getExpirySecs()));
+      if (settings.hasHistory()) out.history(settings.getHistory());
+    }
+
+    return out;
+  }
+
+  private static com.couchbase.client.java.manager.collection.DropCollectionOptions dropCollectionOptions(DropCollectionOptions options, ConcurrentHashMap<String, RequestSpan> spans) {
+    var out = com.couchbase.client.java.manager.collection.DropCollectionOptions.dropCollectionOptions();
+
+    if (options.hasTimeoutMsecs()) out.timeout(java.time.Duration.ofMillis(options.getTimeoutMsecs()));
+    if (options.hasParentSpanId()) out.parentSpan(spans.get(options.getParentSpanId()));
+
+    return out;
   }
 
 }
