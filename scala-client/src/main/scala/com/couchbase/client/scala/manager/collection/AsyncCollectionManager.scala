couchbase/couchbase-jvm-clients

commit 3340f1a9e72ecf54174413470150fe34722ba388

SCBC-387: Convert Scala to use CoreCollectionManager
Moving to CoreCollectionManager allows removing a lot
of code, and support for Protostellar.

Change-Id: I1d5524b700371683d6ba75c72ee6d79c820bd8ae
Reviewed-on: https://review.couchbase.org/c/couchbase-jvm-clients/+/185797
Reviewed-by: David Nault <david.nault@couchbase.com>
Tested-by: Build Bot <build@couchbase.com>

scala-client/src/main/scala/com/couchbase/client/scala/manager/collection/AsyncCollectionManager.scala
@@ -16,31 +16,24 @@
 package com.couchbase.client.scala.manager.collection
 
 import com.couchbase.client.core.retry.RetryStrategy
+import com.couchbase.client.scala.AsyncBucket
+import com.couchbase.client.scala.util.CoreCommonConverters.makeCommonOptions
+import com.couchbase.client.scala.util.DurationConversions.javaDurationToScala
+import com.couchbase.client.scala.util.FutureConversions
 
-import scala.concurrent.Future
 import scala.concurrent.duration.Duration
+import scala.concurrent.{ExecutionContext, Future}
+import scala.jdk.CollectionConverters._
 
-class AsyncCollectionManager(reactive: ReactiveCollectionManager) {
-  private val bucket                       = reactive.bucket
-  private val core                         = bucket.core
-  private[scala] val defaultManagerTimeout = reactive.defaultManagerTimeout
-  private[scala] val defaultRetryStrategy  = reactive.defaultRetryStrategy
-
-  private[scala] def collectionExists(
-      collection: CollectionSpec,
-      timeout: Duration = defaultManagerTimeout,
-      retryStrategy: RetryStrategy = defaultRetryStrategy
-  ): Future[Boolean] = {
-    reactive.collectionExists(collection, timeout, retryStrategy).toFuture
-  }
-
-  private[scala] def scopeExists(
-      scopeName: String,
-      timeout: Duration = defaultManagerTimeout,
-      retryStrategy: RetryStrategy = defaultRetryStrategy
-  ): Future[Boolean] = {
-    reactive.scopeExists(scopeName, timeout, retryStrategy).toFuture
-  }
+class AsyncCollectionManager(private val bucket: AsyncBucket)(
+    implicit val ec: ExecutionContext
+) {
+  private val core = bucket.core
+  private[scala] val defaultManagerTimeout = javaDurationToScala(
+    core.context().environment().timeoutConfig().managementTimeout()
+  )
+  private[scala] val defaultRetryStrategy = core.context().environment().retryStrategy()
+  private val coreCollectionManager       = core.collectionManager(bucket.name)
 
   @deprecated(message = "use getAllScopes instead", since = "1.1.2")
   def getScope(
@@ -48,14 +41,30 @@ class AsyncCollectionManager(reactive: ReactiveCollectionManager) {
       timeout: Duration = defaultManagerTimeout,
       retryStrategy: RetryStrategy = defaultRetryStrategy
   ): Future[ScopeSpec] = {
-    reactive.getScope(scopeName, timeout, retryStrategy).toFuture
+    getAllScopes(timeout, retryStrategy)
+      .map(scopes => scopes.filter(v => v.name == scopeName).head)
   }
 
   def getAllScopes(
       timeout: Duration = defaultManagerTimeout,
       retryStrategy: RetryStrategy = defaultRetryStrategy
   ): Future[Seq[ScopeSpec]] = {
-    reactive.getAllScopes(timeout, retryStrategy).collectSeq().toFuture
+    FutureConversions
+      .javaCFToScalaFutureMappingExceptions(
+        coreCollectionManager.getAllScopes(makeCommonOptions(timeout, retryStrategy))
+      )
+      .map(
+        v =>
+          v.scopes()
+            .asScala
+            .map(
+              scope =>
+                ScopeSpec(
+                  scope.name,
+                  scope.collections.asScala.map(coll => CollectionSpec(coll.name, scope.name))
+                )
+            )
+      )
   }
 
   def createCollection(
@@ -63,7 +72,16 @@ class AsyncCollectionManager(reactive: ReactiveCollectionManager) {
       timeout: Duration = defaultManagerTimeout,
       retryStrategy: RetryStrategy = defaultRetryStrategy
   ): Future[Unit] = {
-    reactive.createCollection(collection, timeout, retryStrategy).toFuture
+    FutureConversions
+      .javaCFToScalaFutureMappingExceptions(
+        coreCollectionManager.createCollection(
+          collection.scopeName,
+          collection.name,
+          null,
+          makeCommonOptions(timeout, retryStrategy)
+        )
+      )
+      .map(_ => ())
   }
 
   def dropCollection(
@@ -71,7 +89,15 @@ class AsyncCollectionManager(reactive: ReactiveCollectionManager) {
       timeout: Duration = defaultManagerTimeout,
       retryStrategy: RetryStrategy = defaultRetryStrategy
   ): Future[Unit] = {
-    reactive.dropCollection(collection, timeout, retryStrategy).toFuture
+    FutureConversions
+      .javaCFToScalaFutureMappingExceptions(
+        coreCollectionManager.dropCollection(
+          collection.scopeName,
+          collection.name,
+          makeCommonOptions(timeout, retryStrategy)
+        )
+      )
+      .map(_ => ())
   }
 
   def createScope(
@@ -79,7 +105,14 @@ class AsyncCollectionManager(reactive: ReactiveCollectionManager) {
       timeout: Duration = defaultManagerTimeout,
       retryStrategy: RetryStrategy = defaultRetryStrategy
   ): Future[Unit] = {
-    reactive.createScope(scopeName, timeout, retryStrategy).toFuture
+    FutureConversions
+      .javaCFToScalaFutureMappingExceptions(
+        coreCollectionManager.createScope(
+          scopeName,
+          makeCommonOptions(timeout, retryStrategy)
+        )
+      )
+      .map(_ => ())
   }
 
   def dropScope(
@@ -87,6 +120,13 @@ class AsyncCollectionManager(reactive: ReactiveCollectionManager) {
       timeout: Duration = defaultManagerTimeout,
       retryStrategy: RetryStrategy = defaultRetryStrategy
   ): Future[Unit] = {
-    reactive.dropScope(scopeName, timeout, retryStrategy).toFuture
+    FutureConversions
+      .javaCFToScalaFutureMappingExceptions(
+        coreCollectionManager.dropScope(
+          scopeName,
+          makeCommonOptions(timeout, retryStrategy)
+        )
+      )
+      .map(_ => ())
   }
 }

scala-client/src/integrationTest/scala/com/couchbase/client/scala/analytics/ScopeLevelAnalyticsSpec.scala
@@ -17,6 +17,7 @@ package com.couchbase.client.scala.analytics
 
 import com.couchbase.client.core.error.{DataverseExistsException, ScopeNotFoundException}
 import com.couchbase.client.core.service.ServiceType
+import com.couchbase.client.core.util.ConsistencyUtil
 import com.couchbase.client.scala.json.JsonObject
 import com.couchbase.client.scala.manager.analytics.AnalyticsIndexManager
 import com.couchbase.client.scala.manager.collection.{CollectionManager, CollectionSpec}
@@ -58,19 +59,13 @@ class ScopeLevelAnalyticsSpec extends ScalaIntegrationTest {
     println("Creating scope and waiting for it to exist..")
 
     bucket.collections.createScope(scopeName).get
-    Util.waitUntilCondition(() => {
-      val result = bucket.collections.scopeExists(scopeName)
-      result.isSuccess && result.get
-    })
+    ConsistencyUtil.waitUntilScopePresent(cluster.async.core, bucket.name, scopeName)
 
     println("Creating collection and waiting for it to exist")
 
     val collSpec = CollectionSpec(collectionName, scopeName)
     bucket.collections.createCollection(collSpec).get
-    Util.waitUntilCondition(() => {
-      val result = bucket.collections.collectionExists(collSpec)
-      result.isSuccess && result.get
-    })
+    ConsistencyUtil.waitUntilCollectionPresent(cluster.async.core, bucket.name, scopeName, collectionName)
 
     scope = bucket.scope(scopeName)
     coll = scope.collection(collectionName)

scala-client/src/integrationTest/scala/com/couchbase/client/scala/manager/CollectionManagerSpec.scala
@@ -58,30 +58,10 @@ class CollectionManagerSpec extends ScalaIntegrationTest {
 
   def waitForScopeToExist(scope: String) = {
     ConsistencyUtil.waitUntilScopePresent(cluster.async.core, bucketName, scope)
-    Util.waitUntilCondition(() => {
-      val result = collections.scopeExists(scope)
-
-      println(s"Waiting for scope ${scope} to exist, result: ${result}")
-
-      result match {
-        case Success(true) => true
-        case _             => false
-      }
-    })
   }
 
   def waitForScopeToNotExist(scope: String) = {
     ConsistencyUtil.waitUntilScopeDropped(cluster.async.core, bucketName, scope)
-    Util.waitUntilCondition(() => {
-      val result = collections.scopeExists(scope)
-
-      println(s"Waiting for scope ${scope} to not exist, result: ${result}")
-
-      result match {
-        case Success(false) => true
-        case _              => false
-      }
-    })
   }
 
   def waitForCollectionToExist(collSpec: CollectionSpec): Unit = {
@@ -91,16 +71,6 @@ class CollectionManagerSpec extends ScalaIntegrationTest {
       collSpec.scopeName,
       collSpec.name
     )
-    Util.waitUntilCondition(() => {
-      val result = collections.collectionExists(collSpec)
-
-      println(s"Waiting for collection ${collSpec} to exist, result: ${result}")
-
-      result match {
-        case Success(true) => true
-        case _             => false
-      }
-    })
   }
 
   def waitForCollectionToNotExist(collSpec: CollectionSpec): Unit = {
@@ -110,23 +80,11 @@ class CollectionManagerSpec extends ScalaIntegrationTest {
       collSpec.scopeName,
       collSpec.name
     )
-    Util.waitUntilCondition(() => {
-      val result = collections.collectionExists(collSpec)
-
-      println(s"Waiting for collection ${collSpec} to not exist, result: ${result}")
-
-      result match {
-        case Success(false) => true
-        case _              => false
-      }
-    })
   }
 
   @Test
   def createScope(): Unit = {
     val scope = randomString
-
-    assert(!collections.scopeExists(scope).get)
 
     collections.createScope(scope).get
 
@@ -138,8 +96,6 @@ class CollectionManagerSpec extends ScalaIntegrationTest {
     val scope      = randomString
     val collection = randomString
     val collSpec   = CollectionSpec(collection, scope)
-
-    assert(!collections.collectionExists(collSpec).get)
 
     collections.createCollection(collSpec) match {
       case Success(_)                           => assert(false)

scala-client/src/integrationTest/scala/com/couchbase/client/scala/query/ScopeLevelQuerySpec.scala
@@ -16,6 +16,7 @@
 package com.couchbase.client.scala.query
 
 import com.couchbase.client.core.service.ServiceType
+import com.couchbase.client.core.util.ConsistencyUtil
 import com.couchbase.client.scala.json.JsonObject
 import com.couchbase.client.scala.kv.MutationState
 import com.couchbase.client.scala.manager.collection.CollectionSpec
@@ -53,23 +54,18 @@ class ScopeLevelQuerySpec extends ScalaIntegrationTest {
     println("Creating scope and waiting for it to exist.")
 
     bucket.collections.createScope(ScopeName).get
-    Util.waitUntilCondition(() => {
-      val result    = bucket.collections.scopeExists(ScopeName)
-      val allScopes = bucket.collections.getAllScopes().get
-
-      println(s"Scope exists result: ${result}, all scopes = ${allScopes}")
-
-      result.isSuccess && result.get
-    })
+    ConsistencyUtil.waitUntilScopePresent(cluster.async.core, bucket.name, ScopeName)
 
     println("Creating collection and waiting for it to exist")
 
     val collSpec = CollectionSpec(CollectionName, ScopeName)
     bucket.collections.createCollection(collSpec).get
-    Util.waitUntilCondition(() => {
-      val result = bucket.collections.collectionExists(collSpec)
-      result.isSuccess && result.get
-    })
+    ConsistencyUtil.waitUntilCollectionPresent(
+      cluster.async.core,
+      bucket.name,
+      ScopeName,
+      CollectionName
+    )
 
     scope = bucket.scope(ScopeName)
     coll = scope.collection(CollectionName)

scala-client/src/main/scala/com/couchbase/client/scala/AsyncBucket.scala
@@ -52,7 +52,7 @@ class AsyncBucket private[scala] (
   private[scala] implicit val ec: ExecutionContext = environment.ec
   val reactive                                     = new ReactiveBucket(this)
 
-  lazy val collections = new AsyncCollectionManager(reactive.collections)
+  lazy val collections = new AsyncCollectionManager(this)
 
   lazy val viewIndexes = new AsyncViewIndexManager(reactive.viewIndexes)
 

scala-client/src/main/scala/com/couchbase/client/scala/Bucket.scala
@@ -61,7 +61,7 @@ class Bucket private[scala] (val async: AsyncBucket) {
   /** Provides a reactive version of this API. */
   lazy val reactive: ReactiveBucket = new ReactiveBucket(async)
 
-  lazy val collections = new CollectionManager(reactive.collections)
+  lazy val collections = new CollectionManager(async.collections)
 
   lazy val viewIndexes = new ViewIndexManager(reactive.viewIndexes)
 

scala-client/src/main/scala/com/couchbase/client/scala/ReactiveBucket.scala
@@ -49,7 +49,7 @@ class ReactiveBucket private[scala] (val async: AsyncBucket) {
   private[scala] val hp                            = HandlerBasicParams(async.core, async.environment)
   private[scala] val viewHandler                   = new ViewHandler(hp)
 
-  lazy val collections = new ReactiveCollectionManager(async)
+  lazy val collections = new ReactiveCollectionManager(async.collections)
 
   lazy val viewIndexes = new ReactiveViewIndexManager(async.core, async.name)
 

scala-client/src/main/scala/com/couchbase/client/scala/manager/bucket/AsyncBucketManager.scala
@@ -21,6 +21,7 @@ import com.couchbase.client.scala.util.DurationConversions._
 
 import scala.concurrent.{ExecutionContext, Future}
 import scala.concurrent.duration.Duration
+
 @Volatile
 class AsyncBucketManager(val reactive: ReactiveBucketManager)(implicit val ec: ExecutionContext) {
   private[scala] val defaultManagerTimeout = reactive.defaultManagerTimeout

commit 8969d03f18d6c55195f529eb0ba43a4c0cf51b03

SCBC-436 CollectionManager.createCollection(CollectionSpec) ignores expiry
Motivation
----------
The [now deprecated] {Async,Reactive}CollectionManager.createCollection
wasn't passing the CollectionSpec's `expiry` value to the server.
This looks like a regression in commit 3340f1a9e72ecf5 for SCBC-387.

Modifications
-------------
Restore the old behavior by passing the expiry on to the core manager.

Change-Id: I722bebd5089414d8f30a4581c976c13f3a1c9297
Reviewed-on: https://review.couchbase.org/c/couchbase-jvm-clients/+/201389
Reviewed-by: Graham Pople <graham.pople@couchbase.com>
Tested-by: Build Bot <build@couchbase.com>

scala-client/src/main/scala/com/couchbase/client/scala/manager/collection/AsyncCollectionManager.scala
@@ -88,7 +88,7 @@ class AsyncCollectionManager(private val bucket: AsyncBucket)(
         coreCollectionManager.createCollection(
           collection.scopeName,
           collection.name,
-          CreateCollectionSettings().toCore,
+          CreateCollectionSettings(maxExpiry = collection.expiry).toCore,
           makeCommonOptions(timeout, retryStrategy)
         )
       )