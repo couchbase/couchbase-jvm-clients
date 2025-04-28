a6d19ed39e0dee7183ab4596aae2e425dab5eadd
David Nault <david.nault@couchbase.com>
2025-04-28 13:07:32 -0700

    JVMCBC-1638 Publish via GitHub Actions
    Modifications
    -------------
    Add GitHub Actions for publishing snapshots and releases.
    
    Automatically publish a snapshot when something
    is pushed to master.
    
    The release action is initiated by a human who specifies
    an existing tag to release.
    
    Use maven-enforcer-plugin to require:
    - Release version does not have -SNAPSHOT suffix
    
    - Snapshot version DOES have -SNAPSHOT suffix
    
    - Either the `snapshot` or `release` profile
      is active when deploying.
    
    - When deploying, it is an error to specify
      an unrecognized profile.
    
    Change-Id: I98603f41bd3eaebcaec0ca118f5db0dbeb7ab8a3
    Reviewed-on: https://review.couchbase.org/c/couchbase-jvm-clients/+/227011
    Reviewed-by: Michael Reiche <michael.reiche@couchbase.com>
    Tested-by: David Nault <david.nault@couchbase.com>

---
+++ .github/scripts/deploy.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+# CI script for publishing to Maven Central.
+
+if [ $# -ne 1 ]; then
+  echo 1>&2 "$0: requires exactly one argument, the Maven profile (snapshot or release)"
+  exit 2
+fi
+
+MAVEN_PROFILE=$1
+
+set -e
+set -x
+
+./mvnw --batch-mode --file protostellar/pom.xml clean install
+./mvnw --batch-mode --file tracing-opentelemetry-deps/pom.xml clean install
+./mvnw --batch-mode --file core-io-deps/pom.xml clean install
+
+# Improper shading should have been caught during PR verification, but let's double check.
+./mvnw --batch-mode --file core-io/pom.xml install -Dmaven.test.skip=true -Dmaven.javadoc.skip=true
+cd core-io ; ./shade-check.sh ; cd ..
+./mvnw --batch-mode --file java-client/pom.xml install -Dmaven.test.skip=true -Dmaven.javadoc.skip=true
+./mvnw --batch-mode --file tracing-opentelemetry/pom.xml install -Dmaven.test.skip=true -Dmaven.javadoc.skip=true
+cd tracing-opentelemetry ; ./shade-check.sh ; cd ..
+
+./mvnw --batch-mode --settings deploy-settings.xml deploy -Dgpg.signer=bc -Dsurefire.rerunFailingTestsCount=1 --activate-profiles ${MAVEN_PROFILE}
+./mvnw --batch-mode --settings deploy-settings.xml clean deploy -Dgpg.signer=bc -Dmaven.test.skip=true --activate-profiles ${MAVEN_PROFILE},scala-2.13 --projects scala-implicits,scala-client
